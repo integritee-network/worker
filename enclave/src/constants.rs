66d863a8f8a5aaa9bb8fd1fb5d023b2c76fc0530
clangenbacher <christian.langenbacher@scs.ch>
2019-05-01 14:43:24 +0200

    [enclave]: wip refactored worker

---
+++ enclave/src/constants.rs
@@ -0,0 +1,20 @@
+/*
+   Copyright 2019 Supercomputing Systems AG
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+pub const RSA3072_SEALED_KEY_FILE: &'static str = "./bin/rsa3072_key_sealed.bin";
+pub const ED25519_SEALED_KEY_FILE: &'static str = "./bin/ed25519_key_sealed.bin";
+pub const COUNTERSTATE:            &'static str = "./bin/sealed_counter_state.bin";
