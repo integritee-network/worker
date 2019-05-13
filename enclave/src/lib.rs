57903c27871024f39d288564b19c4af336b1820b
Marcel Frei <marcel.frei@electronix.ch>
2019-05-13 16:57:04 +0200

    enclave lib: comment unused code

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -307,11 +307,11 @@ struct AllCounts {
     entries: HashMap<String, u8>
 }
 
-fn to_sealed_log<T: Copy + ContiguousMemory>(sealed_data: &SgxSealedData<T>, sealed_log: * mut u8, sealed_log_size: u32) -> Option<* mut sgx_sealed_data_t> {
-    unsafe {
-        sealed_data.to_raw_sealed_data_t(sealed_log as * mut sgx_sealed_data_t, sealed_log_size)
-    }
-}
+// fn to_sealed_log<T: Copy + ContiguousMemory>(sealed_data: &SgxSealedData<T>, sealed_log: * mut u8, sealed_log_size: u32) -> Option<* mut sgx_sealed_data_t> {
+//     unsafe {
+//         sealed_data.to_raw_sealed_data_t(sealed_log as * mut sgx_sealed_data_t, sealed_log_size)
+//     }
+// }
 
 fn from_sealed_log<'a, T: Copy + ContiguousMemory>(sealed_log: * mut u8, sealed_log_size: u32) -> Option<SgxSealedData<'a, T>> {
 	unsafe {
