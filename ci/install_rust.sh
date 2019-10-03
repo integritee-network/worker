62b0efded4081fdf8b794c72e1e55c6c43e71576
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2019-10-03 14:18:50 +0200

    try to fix jenkins build

--- ci/install_rust.sh
+++ ci/install_rust.sh
@@ -17,6 +17,9 @@ source $HOME/.cargo/env
 # Install nightly and beta toolchains, but set stable as a default
 rustup default nightly-2019-08-01
 
+# install targets
+rustup target install wasm32-unknown-unknown
+
 # Install aux components, clippy for linter, rustfmt for formatting
 rustup component add clippy --toolchain=nightly-2019-08-01
 rustup component add rustfmt --toolchain=nightly-2019-08-01
