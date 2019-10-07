9dead6eb1b58fe6c7070cf4df6b35e03fcd8992b
Christian Langenbacher <christian.langenbacher@scs.ch>
2019-10-07 11:36:48 +0200

    updated-application crypto to f17d023bbe179f15678ac9989f471c9b18917

--- application-crypto/src/ed25519.rs
+++ application-crypto/src/ed25519.rs
@@ -22,7 +22,7 @@ pub use primitives::ed25519::*;
 
 mod app {
 	use crate::Vec;
-	use crate::key_types::ED25519;
+	use primitives::testing::ED25519;
 	crate::app_crypto!(super, ED25519);
 }
 
@@ -42,7 +42,7 @@ impl RuntimePublic for Public {
 	}
 
 	fn sign<M: AsRef<[u8]>>(&self, key_type: KeyTypeId, msg: &M) -> Option<Self::Signature> {
-		runtime_io::ed25519_sign(key_type, self, msg)
+		runtime_io::ed25519_sign(key_type, self, msg.as_ref())
 	}
 
 	fn verify<M: AsRef<[u8]>>(&self, msg: &M, signature: &Self::Signature) -> bool {
@@ -53,7 +53,14 @@ impl RuntimePublic for Public {
 #[cfg(test)]
 mod tests {
 	use sr_primitives::{generic::BlockId, traits::ProvideRuntimeApi};
-	use primitives::{testing::KeyStore, crypto::Pair, traits::BareCryptoStore as _};
+	use primitives::{
+				testing::{
+						KeyStore,
+						ED25519,
+				},
+				crypto::Pair,
+				traits::BareCryptoStore as _,
+		};
 	use test_client::{
 		TestClientBuilder, DefaultTestClientBuilderExt, TestClientBuilderExt,
 		runtime::{TestAPI, app_crypto::ed25519::{AppPair, AppPublic}},
@@ -67,7 +74,7 @@ mod tests {
 			.test_ed25519_crypto(&BlockId::Number(0))
 			.expect("Tests `ed25519` crypto.");
 
-		let key_pair = keystore.read().ed25519_key_pair(crate::key_types::ED25519, &public.as_ref())
+		let key_pair = keystore.read().ed25519_key_pair(ED25519, &public.as_ref())
 			.expect("There should be at a `ed25519` key in the keystore for the given public key.");
 
 		assert!(AppPair::verify(&signature, "ed25519", &AppPublic::from(key_pair.public())));
