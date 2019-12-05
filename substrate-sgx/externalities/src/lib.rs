db10b92334c288d14643c1b8b209f05aa9c95bd7
Christian Langenbacher <christian.langenbacher@scs.ch>
2019-12-05 16:58:07 +0100

    [substrate-sgx] formatted externalities file

--- substrate-sgx/externalities/src/lib.rs
+++ substrate-sgx/externalities/src/lib.rs
@@ -12,18 +12,18 @@ pub type SgxExternalities = HashMap<Vec<u8>, Vec<u8>>;
 environmental!(ext: SgxExternalities);
 
 pub trait SgxExternalitiesTrait {
-    fn new() -> Self;
+	fn new() -> Self;
 	fn decode(state: Vec<u8>) -> Self;
 	fn encode(self) -> Vec<u8>;
-    fn insert(&mut self, k: Vec<u8>, v: Vec<u8>) -> Option<Vec<u8>>;
-    fn execute_with<R>(&mut self, f: impl FnOnce() -> R) -> R;
+	fn insert(&mut self, k: Vec<u8>, v: Vec<u8>) -> Option<Vec<u8>>;
+	fn execute_with<R>(&mut self, f: impl FnOnce() -> R) -> R;
 }
 
 impl SgxExternalitiesTrait for SgxExternalities {
-    /// Create a new instance of `BasicExternalities`
-    fn new() -> Self {
-        SgxExternalities::default()
-    }
+	/// Create a new instance of `BasicExternalities`
+	fn new() -> Self {
+		SgxExternalities::default()
+	}
 
 	fn decode(state: Vec<u8>) -> Self {
 		let helper = DeSerializeHelper::<SgxExternalities>::new(state);
@@ -35,31 +35,29 @@ impl SgxExternalitiesTrait for SgxExternalities {
 		helper.encode(self).unwrap()
 	}
 
-    /// Insert key/value
-    fn insert(&mut self, k: Vec<u8>, v: Vec<u8>) -> Option<Vec<u8>> {
-        self.insert(k, v)
-    }
+	/// Insert key/value
+	fn insert(&mut self, k: Vec<u8>, v: Vec<u8>) -> Option<Vec<u8>> {
+		self.insert(k, v)
+	}
 
-    /// Execute the given closure while `self` is set as externalities.
-    ///
-    /// Returns the result of the given closure.
-    fn execute_with<R>(&mut self, f: impl FnOnce() -> R) -> R {
-        set_and_run_with_externalities(self, f)
-    }
+	/// Execute the given closure while `self` is set as externalities.
+	///
+	/// Returns the result of the given closure.
+	fn execute_with<R>(&mut self, f: impl FnOnce() -> R) -> R {
+		set_and_run_with_externalities(self, f)
+	}
 }
 
 /// Set the given externalities while executing the given closure. To get access to the externalities
 /// while executing the given closure [`with_externalities`] grants access to them. The externalities
 /// are only set for the same thread this function was called from.
-pub fn set_and_run_with_externalities<F, R>(ext: &mut SgxExternalities, f: F) -> R
-                                            where F: FnOnce() -> R
-{
-    ext::using(ext, f)
+pub fn set_and_run_with_externalities<F: FnOnce() -> R, R>(ext: &mut SgxExternalities, f: F) -> R {
+	ext::using(ext, f)
 }
 
 /// Execute the given closure with the currently set externalities.
 ///
 /// Returns `None` if no externalities are set or `Some(_)` with the result of the closure.
 pub fn with_externalities<F: FnOnce(&mut SgxExternalities) -> R, R>(f: F) -> Option<R> {
-    ext::with(f)
+	ext::with(f)
 }
