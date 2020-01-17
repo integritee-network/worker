7520c2b7b85681782c22ba2284d16cdacfa4787b
Marcel Frei <marcel.frei@electronix.ch>
2020-01-17 11:26:19 +0100

    install_rust.sh: Switch to nightly-2019-11-17

--- ci/install_rust.sh
+++ ci/install_rust.sh
@@ -14,15 +14,16 @@ chmod +x ./rustup.sh
 # Load new environment
 source $HOME/.cargo/env
 
-# Install nightly and beta toolchains, but set stable as a default
-rustup default nightly-2019-08-01
+# Install and set specific nightly version as default
+rustup install nightly-2019-11-17
+rustup default nightly-2019-11-17
 
 # install targets
 rustup target install wasm32-unknown-unknown
 
 # Install aux components, clippy for linter, rustfmt for formatting
-rustup component add clippy --toolchain=nightly-2019-08-01
-rustup component add rustfmt --toolchain=nightly-2019-08-01
+rustup component add clippy --toolchain=nightly-2019-11-17
+rustup component add rustfmt --toolchain=nightly-2019-11-17
 
 # Show the installed versions
 rustup show
