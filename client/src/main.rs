eb21b49e3177e1cc88cdd3bf719fcd97e6decf15
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-02-22 07:57:50 +0100

    first version of new cli client taken from encointer-node/client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,165 +1,279 @@
-/*
-    Copyright 2019 Supercomputing Systems AG
+//  Copyright (c) 2019 Alain Brenzikofer
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+//! an RPC client to substraTEE using websockets
+//! 
+//! examples
+//! substratee_client 127.0.0.1:9944 transfer //Alice 5G9RtsTbiYJYQYMHbWfyPoeuuxNaCbC16tZ2JGrZ4gRKwz14 1000
+//! 
+#![feature(rustc_private)]
 
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-*/
+#[macro_use]
+extern crate clap;
+#[macro_use] 
+extern crate log;
+extern crate env_logger;
 
 extern crate chrono;
-use chrono::prelude::DateTime;
-use chrono::Utc;
-use std::time::{Duration, UNIX_EPOCH};
+use chrono::{DateTime, Utc};
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
 
-use sgx_types::*;
-
-use base58::ToBase58;
-use clap::{load_yaml, App};
-use codec::Encode;
 use keyring::AccountKeyring;
-use log::*;
-use primitives::{crypto::Ss58Codec, Pair};
-use runtime_primitives::{traits::Verify, AnySignature};
-use serde_derive::{Deserialize, Serialize};
-use substrate_api_client::{utils::hexstr_to_u256, Api};
+use keystore::Store;
+use std::path::PathBuf;
+use app_crypto::{AppKey, AppPublic, AppPair, ed25519, sr25519};
 
-use substratee_client::*;
-use substratee_node_calls::{get_worker_amount, get_worker_info};
-use substratee_stf::{ShardIdentifier, TrustedCall, TrustedGetter, TrustedGetterSigned};
-use substratee_worker_api::Api as WorkerApi;
+use substrate_api_client::{
+    Api, node_metadata,
+    compose_extrinsic,
+    extrinsic, 
+    extrinsic::xt_primitives::{UncheckedExtrinsicV4, GenericAddress},
+    rpc::json_req,
+    utils::{storage_key_hash, hexstr_to_hash, hexstr_to_u256, hexstr_to_u64, hexstr_to_vec},
+};
+use codec::{Encode, Decode};
+use primitives::{
+	crypto::{set_default_ss58_version, Ss58AddressFormat, Ss58Codec},
+	Pair, sr25519 as sr25519_core, Public, H256, hexdisplay::HexDisplay,
+};
+use bip39::{Mnemonic, Language, MnemonicType};
+use base58::{FromBase58, ToBase58};
+use substratee_node_runtime::{AccountId, Event, Call, SubstraTEERegistryCall, BalancesCall, 
+    Signature, Hash,
+    substratee_registry::{Enclave, ShardIdentifier, Request}
+}; 
+use sr_primitives::traits::{Verify, IdentifyAccount};
 
-type AccountId = <AnySignature as Verify>::Signer;
+use serde_json;
+use log::{info, debug, trace, warn};
+use log::Level;
+use clap::App;
+use std::sync::mpsc::channel;
+use std::collections::HashMap;
+
+type AccountPublic = <Signature as Verify>::Signer;
+const KEYSTORE_PATH: &str = "my_keystore";
+const PREFUNDING_AMOUNT: u128 = 1_000_000_000;
 
 fn main() {
-    // message structure
-    #[derive(Debug, Serialize, Deserialize)]
-    struct Message {
-        account: String,
-        amount: u32,
-        sha256: sgx_sha256_hash_t,
+    env_logger::init();
+    let yml = load_yaml!("cli.yml");
+	let matches = App::from_yaml(yml).get_matches();
+
+	let url = matches.value_of("URL").expect("must specify URL");
+	info!("connecting to {}", url);
+    let api = Api::<sr25519::Pair>::new(format!("ws://{}", url));
+    
+    if let Some(_matches) = matches.subcommand_matches("print_metadata") {
+        let meta = api.get_metadata();
+        println!(
+            "Metadata:\n {}",
+            node_metadata::pretty_format(&meta).unwrap()
+        );
     }
 
-    env_logger::init();
+    if let Some(_matches) = matches.subcommand_matches("new-account") {
+        // open store without password protection
+        let store = Store::open(PathBuf::from(&KEYSTORE_PATH), None).unwrap();
+        let key: sr25519::AppPair = store.write().generate().unwrap();
+        drop(store);
+        println!("{}", key.public().to_ss58check())
+    }
 
-    let yml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yml).get_matches();
+    if let Some(_matches) = matches.subcommand_matches("fund-account") {
+        let account = _matches.value_of("account").unwrap();
+        let accountid = get_accountid_from_str(account);
 
-    let port = matches.value_of("node-ws-port").unwrap_or("9944");
-    let server = matches.value_of("node-addr").unwrap_or("127.0.0.1");
+        let _api = api.clone().set_signer(AccountKeyring::Alice.pair());
+        let xt = _api.balance_transfer(GenericAddress::from(accountid.clone()), PREFUNDING_AMOUNT);
+        info!("[+] Alice is generous and pre funds account {}\n", accountid.to_ss58check()); 
+        let tx_hash = _api.send_extrinsic(xt.hex_encode()).unwrap();
+        info!("[+] Pre-Funding transaction got finalized. Hash: {:?}\n", tx_hash);
+        let result = _api.get_free_balance(&accountid.clone());
+        println!("balance for {} is now {}", accountid.to_ss58check(), result);
+    }
 
-    info!("initializing ws api to node");
-    let alice = AccountKeyring::Alice.pair();
-    info!(
-        "use Alice account as signer = {}",
-        alice.public().to_ss58check()
-    );
-    let api = Api::new(format!("ws://{}:{}", server, port)).set_signer(alice.clone());
-
-    println!("*** Getting the amount of the registered workers");
-    let worker = match get_worker_amount(&api) {
-        0 => {
-            println!("No worker in registry, returning...");
-            return;
+    if let Some(_matches) = matches.subcommand_matches("list-accounts") {
+        let store = Store::open(PathBuf::from(&KEYSTORE_PATH), None).unwrap();
+        println!("sr25519 keys:");
+        for pubkey in store.read().public_keys::<sr25519::AppPublic>().unwrap().into_iter() {
+            println!("{}",pubkey.to_ss58check());
         }
-        x => {
-            println!("[<] Found {} workers\n", x);
-            println!("[>] Getting the first worker's info from the substraTEE-node");
-            get_worker_info(&api, 1)
+        println!("ed25519 keys:");
+        for pubkey in store.read().public_keys::<ed25519::AppPublic>().unwrap().into_iter() {
+            println!("{}",pubkey.to_ss58check());
         }
-    };
-    info!("[<] Got first worker's metadata");
-    info!("    worker signing key : {:?}", worker.pubkey.to_string());
-    info!(
-        "    worker url: {}",
-        String::from_utf8_lossy(&worker.url[..]).to_string()
-    );
-    let datetime = DateTime::<Utc>::from(UNIX_EPOCH + Duration::from_secs(worker.timestamp as u64));
-    info!(
-        "    RA timestamp: {}",
-        datetime.format("%Y-%m-%d %H:%M:%S.%f")
-    );
-    info!("    worker mrenclave: {}", worker.mr_enclave.to_base58());
+        drop(store);
+    }
 
-    // default shard is identified by mrenclave
-    let shard = ShardIdentifier::from_slice(&worker.mr_enclave);
+    if let Some(_matches) = matches.subcommand_matches("balance") {
+        let account = _matches.value_of("account").unwrap();
+        let accountid = get_accountid_from_str(account);
+        let result_str = api
+            .get_storage("Balances", "FreeBalance", Some(accountid.encode()))
+            .unwrap();
+        let result = hexstr_to_u256(result_str).unwrap();
+        info!("ss58 is {}", accountid.to_ss58check());
+        println!("balance for {} is {}", account, result);
+    }
 
-    let worker_api = WorkerApi::new(String::from_utf8_lossy(&worker.url[..]).to_string());
+    if let Some(_matches) = matches.subcommand_matches("transfer") {
+        let arg_from = _matches.value_of("from").unwrap();
+        let arg_to = _matches.value_of("to").unwrap();
+        let amount = u128::from_str_radix(_matches.value_of("amount").unwrap(),10).expect("amount can be converted to u128");
+        let from = get_pair_from_str(arg_from);
+        let to = get_accountid_from_str(arg_to);
+        info!("from ss58 is {}", from.public().to_ss58check());
+        info!("to ss58 is {}", to.to_ss58check());
+        let _api = api.clone().set_signer(sr25519_core::Pair::from(from));
+        let xt = _api.balance_transfer(GenericAddress::from(to.clone()), amount);
+        let tx_hash = _api.send_extrinsic(xt.hex_encode()).unwrap();
+        println!("[+] Transaction got finalized. Hash: {:?}\n", tx_hash);
+        let result = _api.get_free_balance(&to);
+        println!("balance for {} is now {}", to, result);
+    }
 
-    info!("getting free_balance for Alice");
-    let result_str = api
-        .get_storage(
-            "Balances",
-            "FreeBalance",
-            Some(AccountId::from(alice.public()).encode()),
-        )
-        .unwrap();
-    let funds = hexstr_to_u256(result_str).unwrap();
-    info!("Alice free balance = {:?}", funds);
-    info!("Alice's Account Nonce is {}", api.get_nonce().unwrap());
+    if let Some(_matches) = matches.subcommand_matches("list-workers") {
+        let wcount = get_enclave_count(&api);
+        println!("number of workers registered: {}", wcount);
+        for w in 1..wcount+1 {
+            let enclave = get_enclave(&api, w);
+            if enclave.is_none() {
+                println!("error reading enclave data");
+                continue;
+            };
+            let enclave = enclave.unwrap();
+            let timestamp = DateTime::<Utc>::from(UNIX_EPOCH + Duration::from_secs(enclave.timestamp as u64));
+            println!("Enclave {}", w);
+            println!("   AccountId: {}", enclave.pubkey.to_ss58check());
+            println!("   MRENCLAVE: {}", enclave.mr_enclave.to_base58());
+            println!("   RA timestamp: {}", timestamp);
+            println!("   URL: {}", String::from_utf8(enclave.url).unwrap());
+        }
+    }
 
-    // compose extrinsic with encrypted payload
-    println!(
-        "[>] Get the shielding key from W1 ({})",
-        worker.pubkey.to_string()
-    );
-    let shielding_pubkey = worker_api.get_rsa_pubkey().unwrap();
-    println!("[<] Got worker shielding key {:?}\n", shielding_pubkey);
-
-    let alice_incognito_pair = pair_from_suri_sr("//AliceIncognito", Some(""));
-    println!(
-        "[+] Alice's Incognito Pubkey: {}\n",
-        alice_incognito_pair.public()
-    );
-
-    let bob_incognito_pair = pair_from_suri_sr("//BobIncognito", Some(""));
-    println!(
-        "[+] Bob's Incognito Pubkey: {}\n",
-        bob_incognito_pair.public()
-    );
-
-    println!("[+] pre-funding Alice's Incognito account (ROOT call)");
-    let call = TrustedCall::balance_set_balance(alice_incognito_pair.public(), 1_000_000, 0);
-    let call_signed = call.sign(&alice_incognito_pair, 0, &worker.mr_enclave, &shard); // for demo we name the shard after our mrenclave
-
-    call_trusted_stf(&api, call_signed, shielding_pubkey, &shard);
-
-    println!("[+] query Alice's Incognito account balance");
-    let getter = TrustedGetter::free_balance(alice_incognito_pair.public());
-    let getter_signed =
-        TrustedGetterSigned::new(getter.clone(), getter.sign(&alice_incognito_pair));
-    get_trusted_stf_state(&worker_api, getter_signed, &shard);
-
-    println!("[+] query Bob's Incognito account balance");
-    let getter = TrustedGetter::free_balance(bob_incognito_pair.public());
-    let getter_signed = TrustedGetterSigned::new(getter.clone(), getter.sign(&bob_incognito_pair));
-    get_trusted_stf_state(&worker_api, getter_signed, &shard);
-
-    println!("*** incognito transfer from Alice to Bob");
-    let call = TrustedCall::balance_transfer(
-        alice_incognito_pair.public(),
-        bob_incognito_pair.public(),
-        100_000,
-    );
-    let call_signed = call.sign(&alice_incognito_pair, 0, &worker.mr_enclave, &shard); // for demo we name the shard after our mrenclave
-    call_trusted_stf(&api, call_signed, shielding_pubkey, &shard);
-
-    println!("[+] query Alice's Incognito account balance");
-    let getter = TrustedGetter::free_balance(alice_incognito_pair.public());
-    let getter_signed =
-        TrustedGetterSigned::new(getter.clone(), getter.sign(&alice_incognito_pair));
-    get_trusted_stf_state(&worker_api, getter_signed, &shard);
-
-    println!("[+] query Bob's Incognito account balance");
-    let getter = TrustedGetter::free_balance(bob_incognito_pair.public());
-    let getter_signed = TrustedGetterSigned::new(getter.clone(), getter.sign(&bob_incognito_pair));
-    get_trusted_stf_state(&worker_api, getter_signed, &shard);
+    if let Some(_matches) = matches.subcommand_matches("listen") {
+        info!("Subscribing to events");
+        let (events_in, events_out) = channel();
+        api.subscribe_events(events_in.clone());
+        loop {
+            let event_str = events_out.recv().unwrap();
+            let _unhex = hexstr_to_vec(event_str).unwrap();
+            let mut _er_enc = _unhex.as_slice();
+            let _events = Vec::<system::EventRecord<Event, Hash>>::decode(&mut _er_enc);
+            match _events {
+                Ok(evts) => {
+                    for evr in &evts {
+                        debug!("decoded: phase {:?} event {:?}", evr.phase, evr.event);
+                        match &evr.event {
+/*                            Event::balances(be) => {
+                                println!(">>>>>>>>>> balances event: {:?}", be);
+                                match &be {
+                                    balances::RawEvent::Transfer(transactor, dest, value, fee) => {
+                                        println!("Transactor: {:?}", transactor);
+                                        println!("Destination: {:?}", dest);
+                                        println!("Value: {:?}", value);
+                                        println!("Fee: {:?}", fee);
+                                    }
+                                    _ => {
+                                        debug!("ignoring unsupported balances event");
+                                    }
+                                }
+                            },*/
+                            Event::substratee_registry(ee) => {
+                                println!(">>>>>>>>>> ceremony event: {:?}", ee);
+                                match &ee {
+                                    substratee_node_runtime::substratee_registry::RawEvent::AddedEnclave(accountid, url) => {
+                                        println!("AddedEnclave: {:?} at url {}", accountid, String::from_utf8(url.to_vec()).unwrap_or("error".to_string()));
+                                    },
+                                    substratee_node_runtime::substratee_registry::RawEvent::RemovedEnclave(accountid) => {
+                                        println!("RemovedEnclave: {:?}", accountid);
+                                    },
+                                    substratee_node_runtime::substratee_registry::RawEvent::UpdatedIpfsHash(shard, idx, ipfs_hash) => {
+                                        println!("UpdatedIpfsHash for shard {}, worker index {}, ipfs# {:?}", shard.encode().to_base58(), idx, ipfs_hash);
+                                    },
+                                    substratee_node_runtime::substratee_registry::RawEvent::Forwarded(request) => {
+                                        let request_hash = hex::encode(request.cyphertext.clone());
+                                        println!("Forwarded request for shard {}: {}", request.shard.encode().to_base58(), request_hash);
+                                    },
+                                    substratee_node_runtime::substratee_registry::RawEvent::CallConfirmed(accountid, call_hash) => {
+                                        println!("CallConfirmed from {} with hash {:?}", accountid, call_hash);
+                                    },
+                                    _ => {
+                                        debug!("ignoring unsupported substraTEE event");
+                                    }
+                                }
+                            },
+                            _ => debug!("ignoring unsupported module event: {:?}", evr.event),
+                        }
+                    }
+                }
+                Err(_) => error!("couldn't decode event record list"),
+            }
+        }
+    }
 }
+
+fn get_accountid_from_str(account: &str) -> AccountId {
+    match &account[..2] {
+        "//" => AccountPublic::from(sr25519::Pair::from_string(account, None)
+            .unwrap().public()).into_account(),
+        _ => AccountPublic::from(sr25519::Public::from_ss58check(account)
+            .unwrap()).into_account(),
+    }
+}
+
+// get a pair either form keyring (well known keys) or from the store
+fn get_pair_from_str(account: &str) ->sr25519::AppPair {
+    info!("getting pair for {}", account);
+    match &account[..2] {
+        "//" => sr25519::AppPair::from_string(account, None).unwrap(),
+        _ => {
+            info!("fetching from keystore at {}", &KEYSTORE_PATH);
+            // open store without password protection
+            let store = Store::open(PathBuf::from(&KEYSTORE_PATH), None).expect("store should exist");
+            info!("store opened");
+            let _pair = store.read().key_pair::<sr25519::AppPair>(&sr25519::Public::from_ss58check(account).unwrap().into()).unwrap();
+            drop(store);
+            _pair
+        }
+            
+    }
+}
+
+fn get_enclave_count(api: &Api<sr25519::Pair>) -> u64 {
+    hexstr_to_u64(api
+            .get_storage("substraTEERegistry", "EnclaveCount", None)
+            .unwrap()
+            ).unwrap() 
+}
+
+fn get_enclave(
+    api: &Api<sr25519::Pair>, 
+    eindex: u64, 
+    ) -> Option<Enclave<AccountId, Vec<u8>>> 
+{
+    let res = api
+        .get_storage("substraTEERegistry", "EnclaveRegistry", 
+            Some(eindex.encode())).unwrap();
+    match res.as_str() {
+        "null" => None,
+        _ => {
+            let enclave: Enclave<AccountId, Vec<u8>> = Decode::decode(
+                &mut &hexstr_to_vec(res).unwrap()[..]).unwrap();
+            Some(enclave)
+        }
+    }
+}
+
