e80d845d6affadb17bcfe17e29f484af0eadd6d3
Christian Langenbacher <christian.langenbacher@scs.ch>
2020-04-23 07:53:28 +0200

    [UPDATERUSTSGXSDK] fix to release v1.1.1

--- UpdateRustSGXSDK.mk
+++ UpdateRustSGXSDK.mk
@@ -11,21 +11,25 @@ LOCAL_VERSION = $(shell cat $(VERSION_FILE))
 COMMAND = git ls-remote $(REPO) HEAD | awk '{ print $$1 }'
 REMOTE_VERSION = $(shell $(COMMAND))
 
+# version vor sgx-sdk v1.1.1 release
+VERSION111 = 66aaa2888992c63137e87adc688ddedab1181056
+
 # update the SDK files
 all: updatesdk
 
 updatesdk:
 # check for already updated version
-ifneq ('$(LOCAL_VERSION)','$(REMOTE_VERSION)')
+ifneq ('$(VERSION111)','$(LOCAL_VERSION)')
 	@echo Local version = $(LOCAL_VERSION)
-	@echo Remote version = $(REMOTE_VERSION)
+	@echo sgxsdk v1.1.1 version = $(VERSION111)
 
 	@rm -rf $(SDK_PATH_GIT)
 	@$(GIT) clone $(REPO) $(SDK_PATH_GIT)
+	@cd $(SDK_PATH_GIT) && $(GIT) checkout $(VERSION111) && cd ../
 	rsync -a $(SDK_PATH_GIT)/edl $(SDK_PATH)
 	rsync -a $(SDK_PATH_GIT)/common $(SDK_PATH)
 	rsync -a $(SDK_PATH_GIT)/compiler-rt $(SDK_PATH)
 	rm -rf $(SDK_PATH_GIT)
-	@echo $(REMOTE_VERSION) > $(VERSION_FILE)
+	@echo $(VERSION111) > $(VERSION_FILE)
 
 endif
