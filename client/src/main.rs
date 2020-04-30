f5c1aca72dc116ee3c4db92905b510919233b460
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-04-30 08:56:37 +0200

    client fixes

--- client/src/main.rs
+++ client/src/main.rs
@@ -559,11 +559,11 @@ fn get_pair_from_str(account: &str) -> sr25519::AppPair {
 }
 
 fn get_enclave_count(api: &Api<sr25519::Pair>) -> u64 {
-    api.get_storage_value("SubstrateeRegistry", "EnclaveCount")
-        .unwrap()
+    if let Some(count) = api.get_storage_value("SubstrateeRegistry", "EnclaveCount") {
+        count
+    } else { 0 }
 }
 
 fn get_enclave(api: &Api<sr25519::Pair>, eindex: u64) -> Option<Enclave<AccountId, Vec<u8>>> {
     api.get_storage_map("SubstrateeRegistry", "EnclaveRegistry", eindex)
-        .unwrap()
 }
