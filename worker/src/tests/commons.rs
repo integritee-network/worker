6f7176953a241ed4fe11a697dead142f3f821134
Christian Langenbacher <christian.langenbacher@scs.ch>
2020-05-27 09:54:43 +0200

    [worker/enclave_api] enclave_shielding_key now returns Rsa3072PubKey instead of Vec<u8>

--- worker/src/tests/commons.rs
+++ worker/src/tests/commons.rs
@@ -42,9 +42,7 @@ pub struct Message {
 /// Who must be root account
 pub fn encrypted_set_balance(eid: sgx_enclave_id_t, who: AccountKeyring, nonce: u32) -> Vec<u8> {
     info!("*** Get the public key from the TEE\n");
-    let rsa_pubkey: Rsa3072PubKey = enclave_shielding_key(eid)
-        .map(|key| serde_json::from_slice(key.as_slice()).unwrap())
-        .unwrap();
+    let rsa_pubkey: Rsa3072PubKey = enclave_shielding_key(eid).unwrap();
     info!("deserialized rsa key");
 
     let call = TrustedCall::balance_set_balance(who.public(), who.public(), 33, 44);
@@ -62,9 +60,7 @@ pub fn encrypted_set_balance(eid: sgx_enclave_id_t, who: AccountKeyring, nonce: 
 
 pub fn encrypted_unshield(eid: sgx_enclave_id_t, who: AccountKeyring, nonce: u32) -> Vec<u8> {
     info!("*** Get the public key from the TEE\n");
-    let rsa_pubkey: Rsa3072PubKey = enclave_shielding_key(eid)
-        .map(|key| serde_json::from_slice(key.as_slice()).unwrap())
-        .unwrap();
+    let rsa_pubkey: Rsa3072PubKey = enclave_shielding_key(eid).unwrap();
     info!("deserialized rsa key");
 
     let call =
@@ -96,9 +92,7 @@ pub fn test_trusted_getter_signed(who: AccountKeyring) -> TrustedGetterSigned {
 
 pub fn encrypted_alice(eid: sgx_enclave_id_t) -> Vec<u8> {
     info!("*** Get the public key from the TEE\n");
-    let rsa_pubkey: Rsa3072PubKey = enclave_shielding_key(eid)
-        .map(|key| serde_json::from_slice(key.as_slice()).unwrap())
-        .unwrap();
+    let rsa_pubkey: Rsa3072PubKey = enclave_shielding_key(eid).unwrap();
     encrypt_payload(rsa_pubkey, AccountKeyring::Alice.encode())
 }
 
