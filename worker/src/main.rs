15fc7cda48bf0379d1e0423aaef9df2181dc4723
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-09-09 09:13:49 +0200

    don't request key provisioning form other worker

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -273,27 +273,27 @@ fn worker(w_ip: &str, w_port: &str, mu_ra_port: &str, shard: &ShardIdentifier) {
     let tx_hash = api.send_extrinsic(_xthex, XtStatus::InBlock).unwrap();
     println!("[<] Extrinsic got finalized. Hash: {:?}\n", tx_hash);
 
-    // browse enclave registry
-    match get_first_worker_that_is_not_equal_to_self(&api, &tee_accountid) {
-        Some(w) => {
-            let _url = String::from_utf8_lossy(&w.url[..]).to_string();
-            let _w_api = WorkerApi::new(_url.clone());
-            let _url_split: Vec<_> = _url.split(':').collect();
-            let mura_url = format!("{}:{}", _url_split[0], _w_api.get_mu_ra_port().unwrap());
+    // // browse enclave registry
+    // match get_first_worker_that_is_not_equal_to_self(&api, &tee_accountid) {
+    //     Some(w) => {
+    //         let _url = String::from_utf8_lossy(&w.url[..]).to_string();
+    //         let _w_api = WorkerApi::new(_url.clone());
+    //         let _url_split: Vec<_> = _url.split(':').collect();
+    //         let mura_url = format!("{}:{}", _url_split[0], _w_api.get_mu_ra_port().unwrap());
 
-            info!("Requesting key provisioning from worker at {}", mura_url);
-            enclave_request_key_provisioning(
-                eid,
-                sgx_quote_sign_type_t::SGX_UNLINKABLE_SIGNATURE,
-                &mura_url,
-            )
-            .unwrap();
-            debug!("key provisioning successfully performed");
-        }
-        None => {
-            info!("there are no other workers");
-        }
-    }
+    //         info!("Requesting key provisioning from worker at {}", mura_url);
+    //         enclave_request_key_provisioning(
+    //             eid,
+    //             sgx_quote_sign_type_t::SGX_UNLINKABLE_SIGNATURE,
+    //             &mura_url,
+    //         )
+    //         .unwrap();
+    //         debug!("key provisioning successfully performed");
+    //     }
+    //     None => {
+    //         info!("there are no other workers");
+    //     }
+    // }
 
     let mut latest_head = init_chain_relay(eid, &api);
 
