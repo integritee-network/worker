8a388cec5653f796e9aee3c6f14c13d1d5ad30d0
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-03-15 15:37:38 +0100

    some more updates, not yet compiling

--- worker/src/tests/commons.rs
+++ worker/src/tests/commons.rs
@@ -106,7 +106,7 @@ pub fn setup(
     port: &str,
 ) -> (Api<sr25519::Pair>, Option<u32>, ShardIdentifier) {
     let node_url = format!("ws://{}:{}", "127.0.0.1", port);
-    let mut api = Api::<sr25519::Pair>::new(node_url);
+    let mut api = Api::<sr25519::Pair>::new(node_url).unwrap();
     ensure_account_has_funds(&mut api, &enclave_account(eid));
 
     // create the state such that we do not need to initialize it manually
@@ -126,7 +126,7 @@ pub fn setup(
 }
 
 pub fn get_nonce(api: &Api<sr25519::Pair>, who: &AccountId32) -> u32 {
-    if let Some(info) = api.get_account_info(who) {
+    if let Some(info) = api.get_account_info(who).unwrap() {
         info.nonce
     } else {
         0
