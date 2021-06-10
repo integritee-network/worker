f7d984e8da77660eb3100f9913e53491ec7d6a2a
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-06-10 11:53:08 +0200

    [node/primitives] add improved enclave definition.

--- primitives/node/src/lib.rs
+++ primitives/node/src/lib.rs
@@ -17,10 +17,7 @@ pub type PalletString = Vec<u8>;
 pub type PalletString = String;
 
 #[cfg(feature = "std")]
-pub type SignedBlock = sp_runtime::generic::SignedBlock<my_node_runtime::Block>;
-
-#[cfg(feature = "std")]
-pub use my_node_runtime::substratee_registry::Enclave as EnclaveGen;
+pub use my_node_runtime::SignedBlock;
 
 pub use sp_core::crypto::AccountId32 as AccountId;
 
@@ -34,10 +31,32 @@ pub struct Request {
     pub cyphertext: Vec<u8>,
 }
 
-#[cfg(feature = "std")]
-pub type Enclave = EnclaveGen<AccountId, PalletString>;
 pub type IpfsHash = [u8; 46];
 
 pub type SubstrateeConfirmCallFn = ([u8; 2], ShardIdentifier, H256, Vec<u8>);
 pub type ShieldFundsFn = ([u8; 2], Vec<u8>, u128, ShardIdentifier);
 pub type CallWorkerFn = ([u8; 2], Request);
+
+// Todo: move this improved enclave definition into a primitives crate substratee-registry repo.
+#[derive(Encode, Decode, Default, Clone, PartialEq, sp_core::RuntimeDebug)]
+pub struct EnclaveGen<AccountId> {
+    pub pubkey: AccountId,
+    // FIXME: this is redundant information
+    pub mr_enclave: [u8; 32],
+    pub timestamp: u64,
+    // unix epoch in milliseconds
+    pub url: PalletString, // utf8 encoded url
+}
+
+impl<AccountId> EnclaveGen<AccountId> {
+    pub fn new(pubkey: AccountId, mr_enclave: [u8; 32], timestamp: u64, url: PalletString) -> Self {
+        Self {
+            pubkey,
+            mr_enclave,
+            timestamp,
+            url,
+        }
+    }
+}
+
+pub type Enclave = EnclaveGen<AccountId>;
