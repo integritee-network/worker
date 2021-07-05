344b53796636868c96430fbbc6dfdccbc00d3af0
clangenb <37865735+clangenb@users.noreply.github.com>
2021-07-05 16:51:53 +0200

    Introduce enclave error (#290)
    * introduce custom enclave error.
    * [enclave] define EnclaveResult in error.rs and rename it to plain result.

---
+++ enclave/src/error.rs
@@ -0,0 +1,33 @@
+use derive_more::{Display, From};
+use sgx_types::sgx_status_t;
+use crate::rpc;
+
+use std::result::Result as StdResult;
+
+pub type Result<T> = StdResult<T, Error>;
+
+#[derive(Debug, Display, From)]
+pub enum Error {
+	Rpc(rpc::error::Error),
+	Codec(codec::Error),
+	Sgx(sgx_status_t)
+}
+
+impl From<Error> for sgx_status_t {
+	/// return sgx_status for top level enclave functions
+	fn from(error: Error) -> sgx_status_t {
+		match error {
+			Error::Sgx(status) => status,
+			_=>  {
+				log::warn!("Tried extracting sgx_status from non-sgx error: {:?}", error);
+				sgx_status_t::SGX_ERROR_UNEXPECTED
+			}
+		}
+	}
+}
+
+impl<T> From<Error> for StdResult<T, Error> {
+	fn from(error: Error) -> StdResult<T, Error> {
+		Err(error)
+	}
+}
