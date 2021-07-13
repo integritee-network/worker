07a853ee22ae454528807dd34ab31715bb6619d3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-07-13 13:36:32 +0200

    [local-setup] PoC: read setup from config.json

--- local-setup/launch.py
+++ local-setup/launch.py
@@ -2,6 +2,7 @@
 """
 Launch a local dev setup consisting of one substraTEE-node and two workers.
 """
+import json
 import signal
 from subprocess import Popen, STDOUT
 from time import sleep
@@ -10,42 +11,55 @@ from typing import Union, IO
 from py.worker import Worker
 from py.helpers import GracefulKiller, mkdir_p
 
-log_dir = '../log'
+log_dir = 'log'
 mkdir_p(log_dir)
 node_log = open(f'{log_dir}/node.log', 'w+')
 worker1_log = open(f'{log_dir}/worker1.log', 'w+')
 worker2_log = open(f'{log_dir}/worker2.log', 'w+')
 
-source_bin_folder = '../bin'
-node_bin = '../../integritee-node/target/release/integritee-node'
+source_bin_folder = 'bin'
+node_bin = '../integritee-node/target/release/integritee-node'
 w1_working_dir = '/tmp/w1'
 w2_working_dir = '/tmp/w2'
 
 
-def setup_worker(work_dir: str, std_err: Union[None, int, IO]):
+def setup_worker(work_dir: str, source_dir: str, std_err: Union[None, int, IO]):
     print(f'Setting up worker in {work_dir}')
-    worker = Worker(cwd=work_dir, source_dir=source_bin_folder, std_err=std_err)
+    print(f'Copying files from {source_dir}')
+    worker = Worker(cwd=work_dir, source_dir=source_dir, std_err=std_err)
     worker.init_clean()
     print('Initialized worker.')
     return worker
 
+def run_node(config):
+    node_cmd = [config["node"]["bin"]] + config["node"]["flags"]
+    return Popen(node_cmd, stdout=node_log, stderr=STDOUT, bufsize=1)
+
 
 def main(processes):
     print('Starting substraTee-node-process in background')
-    processes.append(
-        Popen([node_bin, '--tmp', '--dev', '-lruntime=info'], stdout=node_log, stderr=STDOUT, bufsize=1)
-    )
 
-    w1 = setup_worker(w1_working_dir, worker1_log)
-    w2 = setup_worker(w2_working_dir, worker2_log)
+    with open('./local-setup/simple-config.json') as config_file:
+        config = json.load(config_file)
 
-    print('Starting worker 1 in background')
-    processes.append(w1.run_in_background(log_file=worker1_log, flags=['-P', '2000'], subcommand_flags=['--skip-ra']))
+    processes.append(run_node(config))
 
-    # sleep to prevent nonce clash when bootstrapping the enclave's account
-    sleep(6)
-    print('Starting worker 2 in background')
-    processes.append(w2.run_in_background(log_file=worker2_log, flags=['-P', '3000'], subcommand_flags=['--skip-ra']))
+    w_source = config["workers"]["source"]
+
+    i = 1
+    for instance in config["workers"]["instances"]:
+        log = open(f'{log_dir}/worker{i}.log', 'w+')
+        w = setup_worker(f'/tmp/w{i}', w_source, log)
+
+        print(f'Starting worker {i} in background')
+        processes.append(
+            w.run_in_background(log_file=log, flags=instance["flags"], subcommand_flags=instance["subcommand_flags"]))
+
+        # sleep to prevent nonce clash when bootstrapping the enclave's account
+        sleep(6)
+
+        i += 1
+
     # keep script alive until terminated
     signal.pause()
 
