31e775ba256b6a898d711934665fd05553e81a71
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-07-15 13:05:09 +0200

    [scripts] fix if condition

--- scripts/m8.sh
+++ scripts/m8.sh
@@ -4,19 +4,18 @@ set -euo pipefail
 cd "$CLIENT_DIR" || exit
 
 if [[ -z "${LOG_1}" ]] && [[ -z "${LOG_2}" ]]; then
-  # we are in github actions if this exists, then we print to the logs
-
-    echo "We are in ga actions!"
-
-    touch "$LOG_1"
-    touch "$LOG_2"
-
-  ./demo_direct_call.sh -p 9944 -P 2000 -t first  2>&1 | tee "$LOG_1"
-  ./demo_direct_call.sh -p 9944 -P 2000 -t second  2>&1 | tee "$LOG_2"
-else
-
-  echo "NOT IN GA!"
-
   ./demo_direct_call.sh -p 9944 -P 2000 -t first
   ./demo_direct_call.sh -p 9944 -P 2000 -t second
+else
+  # we are in github actions if this exists, then we print to the logs
+
+  echo "[m8.sh] printing to logs:"
+  echo "        $LOG_1"
+  echo "        $LOG_2"
+
+  touch "$LOG_1"
+  touch "$LOG_2"
+
+  ./demo_direct_call.sh -p 9944 -P 2000 -t first 2>&1 | tee "$LOG_1"
+  ./demo_direct_call.sh -p 9944 -P 2000 -t second 2>&1 | tee "$LOG_2"
 fi
