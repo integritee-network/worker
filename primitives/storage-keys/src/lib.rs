a9bbcfc34ae9f3de00c5a9015546ff4b6a721cc9
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-07-29 16:39:46 +0200

    [primitives] add storage-keys crate

---
+++ primitives/storage-keys/src/lib.rs
@@ -0,0 +1,56 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::Encode;
+use frame_metadata::StorageHasher;
+use sp_std::vec::Vec;
+
+pub fn storage_value_key(module_prefix: &str, storage_prefix: &str) -> Vec<u8> {
+	let mut bytes = sp_core::twox_128(module_prefix.as_bytes()).to_vec();
+	bytes.extend(&sp_core::twox_128(storage_prefix.as_bytes())[..]);
+	bytes
+}
+
+pub fn storage_map_key<K: Encode>(
+	module_prefix: &str,
+	storage_prefix: &str,
+	mapkey1: &K,
+	hasher1: &StorageHasher,
+) -> Vec<u8> {
+	let mut bytes = sp_core::twox_128(module_prefix.as_bytes()).to_vec();
+	bytes.extend(&sp_core::twox_128(storage_prefix.as_bytes())[..]);
+	bytes.extend(key_hash(mapkey1, hasher1));
+	bytes
+}
+
+pub fn storage_double_map_key<K: Encode, Q: Encode>(
+	module_prefix: &str,
+	storage_prefix: &str,
+	mapkey1: &K,
+	hasher1: &StorageHasher,
+	mapkey2: &Q,
+	hasher2: &StorageHasher,
+) -> Vec<u8> {
+	let mut bytes = sp_core::twox_128(module_prefix.as_bytes()).to_vec();
+	bytes.extend(&sp_core::twox_128(storage_prefix.as_bytes())[..]);
+	bytes.extend(key_hash(mapkey1, hasher1));
+	bytes.extend(key_hash(mapkey2, hasher2));
+	bytes
+}
+
+/// generates the key's hash depending on the StorageHasher selected
+fn key_hash<K: Encode>(key: &K, hasher: &StorageHasher) -> Vec<u8> {
+	let encoded_key = key.encode();
+	match hasher {
+		StorageHasher::Identity => encoded_key.to_vec(),
+		StorageHasher::Blake2_128 => sp_core::blake2_128(&encoded_key).to_vec(),
+		StorageHasher::Blake2_128Concat => {
+			// copied from substrate Blake2_128Concat::hash since StorageHasher is not public
+			let x: &[u8] = encoded_key.as_slice();
+			sp_core::blake2_128(x).iter().chain(x.iter()).cloned().collect::<Vec<_>>()
+		},
+		StorageHasher::Blake2_256 => sp_core::blake2_256(&encoded_key).to_vec(),
+		StorageHasher::Twox128 => sp_core::twox_128(&encoded_key).to_vec(),
+		StorageHasher::Twox256 => sp_core::twox_256(&encoded_key).to_vec(),
+		StorageHasher::Twox64Concat => sp_core::twox_64(&encoded_key).to_vec(),
+	}
+}
