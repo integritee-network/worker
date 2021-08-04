b6566cab686624aee50eafefe2b9c53170858659
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-04 15:52:25 +0200

    [enclave/onchain_storage] add get multiple storage_values not sure if implementation is good.

--- enclave/src/onchain/storage.rs
+++ enclave/src/onchain/storage.rs
@@ -1,11 +1,24 @@
-use crate::{ocall::ocall_api::EnclaveOnChainOCallApi, Result};
+use crate::{error::Error, ocall::ocall_api::EnclaveOnChainOCallApi, Result};
+use codec::Decode;
 use sp_core::H256;
 use sp_runtime::traits::Header;
-use sp_std::prelude::Vec;
+use sp_std::{fmt::Debug, prelude::Vec};
 use substratee_storage::{verify_storage_entries, Error as StorageError, StorageEntryVerified};
 use substratee_worker_primitives::WorkerRequest;
 
 pub trait GetOnchainStorage {
+	fn get_onchain_storage_value<V: Decode, H: Header<Hash = H256>>(
+		&self,
+		storage_hash: Vec<u8>,
+		header: &H,
+	) -> Result<Option<V>>;
+
+	fn get_multiple_onchain_storage_values<V: Decode + Debug, H: Header<Hash = H256>>(
+		&self,
+		storage_hash: Vec<Vec<u8>>,
+		header: &H,
+	) -> Result<Vec<Option<V>>>;
+
 	fn get_onchain_storage<H: Header<Hash = H256>>(
 		&self,
 		storage_hash: Vec<u8>,
@@ -20,6 +33,41 @@ pub trait GetOnchainStorage {
 }
 
 impl<O: EnclaveOnChainOCallApi> GetOnchainStorage for O {
+	fn get_onchain_storage_value<V: Decode, H: Header<Hash = H256>>(
+		&self,
+		storage_hash: Vec<u8>,
+		header: &H,
+	) -> Result<Option<V>> {
+		Ok(self
+			.get_onchain_storage(storage_hash, header)?
+			.into_tuple()
+			.1
+			.map(|value| Decode::decode(&mut value.as_slice()))
+			.transpose()?)
+	}
+
+	fn get_multiple_onchain_storage_values<V: Decode + Debug, H: Header<Hash = H256>>(
+		&self,
+		storage_hash: Vec<Vec<u8>>,
+		header: &H,
+	) -> Result<Vec<Option<V>>> {
+		let (storages, errors): (Vec<_>, Vec<_>) = self
+			.get_multiple_onchain_storages(storage_hash, header)?
+			.into_iter()
+			.map(|entry| entry.into_tuple().1)
+			.map(|opt| {
+				opt.map(|value| Decode::decode(&mut value.as_slice()).map_err(|e| e.into()))
+					.transpose()
+			})
+			.partition(Result::is_ok);
+
+		for e in errors.into_iter() {
+			log::error!("Storage fetch errors: {:?}", e.unwrap_err())
+		}
+
+		Ok(storages.into_iter().map(Result::unwrap).collect())
+	}
+
 	fn get_onchain_storage<H: Header<Hash = H256>>(
 		&self,
 		storage_hash: Vec<u8>,
