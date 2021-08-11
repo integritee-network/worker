e7f2deaca7be32e22da81c881682325e25dba41d
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-11 13:21:17 +0200

    [ci] update the ci script for new rust-toolchain.toml

--- ci/install_rust.sh
+++ ci/install_rust.sh
@@ -5,8 +5,6 @@
 # Print all executed commands
 set -ex
 
-TOOLCHAIN=$(cat ./rust-toolchain)
-
 # Download rustup script and execute it
 curl https://sh.rustup.rs -sSf > ./rustup.sh
 chmod +x ./rustup.sh
@@ -15,23 +13,8 @@ chmod +x ./rustup.sh
 # Load new environment
 source $HOME/.cargo/env
 
-# Install nightly that supports clippy
-# Overview: https://rust-lang.github.io/rustup-components-history/index.html
-rustup toolchain add $TOOLCHAIN
+# Load new environment
+source $HOME/.cargo/env
 
-# Install aux components, clippy for linter, rustfmt for formatting
-rustup component add clippy --toolchain $TOOLCHAIN
-rustup component add rustfmt --toolchain $TOOLCHAIN
-
-# Install WASM toolchain
-rustup target add wasm32-unknown-unknown --toolchain $TOOLCHAIN
-
-# Install wasm-gc
-if ! [ -x "$(command -v wasm-gc)" ]; then
-    cargo install --git https://github.com/alexcrichton/wasm-gc
-else
-    echo "wasm-gc already installed"
-fi
-
-# Show the installed versions
+# With the new rust-toolchain.toml format, this automatically installs the correct components.
 rustup show
