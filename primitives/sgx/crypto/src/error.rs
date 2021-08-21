21a10819f64363d225ce785156ba4350d35ccecc
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-21 08:55:24 +0200

    [enclave] move ed25591 to primitives/crypto

--- primitives/sgx/crypto/src/error.rs
+++ primitives/sgx/crypto/src/error.rs
@@ -1,10 +1,12 @@
 use derive_more::{Display, From};
+use std::prelude::v1::Box;
 
 #[derive(Debug, Display, From)]
 pub enum Error {
 	IO(std::io::Error),
 	InvalidNonceKeyLength,
 	Codec(codec::Error),
+	Other(Box<dyn std::error::Error>),
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
