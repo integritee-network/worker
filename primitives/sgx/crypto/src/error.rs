fb8355001d3887faf8281aa64a9731aa1590d3ff
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-21 09:06:47 +0200

    [primitives/crypto] Error: implement From<Error> for sgx_status_t

--- primitives/sgx/crypto/src/error.rs
+++ primitives/sgx/crypto/src/error.rs
@@ -1,4 +1,5 @@
 use derive_more::{Display, From};
+use sgx_types::sgx_status_t;
 use std::prelude::v1::Box;
 
 #[derive(Debug, Display, From)]
@@ -10,3 +11,15 @@ pub enum Error {
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
+
+impl From<Error> for sgx_status_t {
+	/// return sgx_status for top level enclave functions
+	fn from(error: Error) -> sgx_status_t {
+		match error {
+			_ => {
+				log::warn!("Transform non-sgx-error into `SGX_ERROR_UNEXPECTED`: {:?}", error);
+				sgx_status_t::SGX_ERROR_UNEXPECTED
+			},
+		}
+	}
+}
