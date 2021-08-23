2ec97b0c73979dba3bd3a7714d0cf67d7b5a4da1
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-23 10:15:01 +0200

    fix clippy

--- primitives/sgx/crypto/src/error.rs
+++ primitives/sgx/crypto/src/error.rs
@@ -15,11 +15,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 impl From<Error> for sgx_status_t {
 	/// return sgx_status for top level enclave functions
 	fn from(error: Error) -> sgx_status_t {
-		match error {
-			_ => {
-				log::warn!("Transform non-sgx-error into `SGX_ERROR_UNEXPECTED`: {:?}", error);
-				sgx_status_t::SGX_ERROR_UNEXPECTED
-			},
-		}
+		log::warn!("Transform non-sgx-error into `SGX_ERROR_UNEXPECTED`: {:?}", error);
+		sgx_status_t::SGX_ERROR_UNEXPECTED
 	}
 }
