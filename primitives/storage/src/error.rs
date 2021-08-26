effff95eff0d850bb7a4c7e84e8dd6403a3628e3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-26 12:21:32 +0200

    [primitives/storage] implement this error for storage, a little bit ugly though

--- primitives/storage/src/error.rs
+++ primitives/storage/src/error.rs
@@ -1,6 +1,34 @@
-use derive_more::{Display, From};
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+use thiserror_sgx as thiserror;
 
-#[derive(Debug, Display, PartialEq, Eq, From)]
+// error with std::error::Error implemented for std and sgx
+#[derive(Debug, thiserror::Error)]
+#[cfg(any(feature = "std", feature = "sgx"))]
+pub enum Error {
+	#[error("No storage proof supplied")]
+	NoProofSupplied,
+	#[error("Supplied storage value does not match the value from the proof")]
+	WrongValue,
+	#[error("Invalid storage proof: StorageRootMismatch")]
+	StorageRootMismatch,
+	#[error("Storage value unavailable")]
+	StorageValueUnavailable,
+	#[error(transparent)]
+	#[cfg(feature = "std")]
+	Codec(#[from] codec::Error),
+	#[error("Codec: {0}")]
+	#[cfg(not(feature = "std"))]
+	Codec(codec::Error),
+}
+
+// error for bare `no_std` below.
+
+#[cfg(all(not(feature = "std"), not(feature = "sgx")))]
+use derive_more::From;
+
+// Simple error enum for no_std without std::error::Error implemented
+#[derive(Debug, From)]
+#[cfg(all(not(feature = "std"), not(feature = "sgx")))]
 pub enum Error {
 	NoProofSupplied,
 	/// Supplied storage value does not match the value from the proof
--- primitives/storage/src/lib.rs
+++ primitives/storage/src/lib.rs
@@ -17,6 +17,12 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(all(feature = "std", feature = "sgx"))]
+compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
+
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+extern crate sgx_tstd as std;
+
 pub use error::Error;
 pub use frame_metadata::StorageHasher;
 pub use keys::*;
