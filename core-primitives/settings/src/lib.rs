61ba608dfda7e60a48c80eb3bf08eba7a1a21efc
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-11-23 08:42:57 +0100

    initial commit

--- core-primitives/settings/src/lib.rs
+++ core-primitives/settings/src/lib.rs
@@ -101,7 +101,7 @@ pub mod node {
 	pub static PROPOSED_SIDECHAIN_BLOCK: u8 = 4u8;
 	pub static SHIELD_FUNDS: u8 = 5u8;
 	// bump this to be consistent with integritee-node runtime
-	pub static RUNTIME_SPEC_VERSION: u32 = 3;
+	pub static RUNTIME_SPEC_VERSION: u32 = 4;
 	pub static RUNTIME_TRANSACTION_VERSION: u32 = 1;
 	pub static UNSHIELD: u8 = 6u8;
 }
