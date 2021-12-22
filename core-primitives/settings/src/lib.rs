419ca570a231fa7ccc6083962ecf269fd10b0b14
echevrier <edith.chevrier@scs.ch>
2021-12-22 16:01:25 +0100

    Update spec verion to 5 and transaction version to 2 Update CI to run with new node

--- core-primitives/settings/src/lib.rs
+++ core-primitives/settings/src/lib.rs
@@ -107,8 +107,8 @@ pub mod node {
 	pub static PROPOSED_SIDECHAIN_BLOCK: u8 = 4u8;
 	pub static SHIELD_FUNDS: u8 = 5u8;
 	// bump this to be consistent with integritee-node runtime
-	pub static RUNTIME_SPEC_VERSION: u32 = 5;
-	pub static RUNTIME_TRANSACTION_VERSION: u32 = 1;
+	pub static RUNTIME_SPEC_VERSION: u32 = 6;
+	pub static RUNTIME_TRANSACTION_VERSION: u32 = 2;
 	pub static UNSHIELD: u8 = 6u8;
 
 	pub static TEERACLE_MODULE: u8 = 52u8;
