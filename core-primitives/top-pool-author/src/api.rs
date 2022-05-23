259fdb4090e667fd696133b11dcace35455a108f
Felix Müller <felix.mueller@scs.ch>
2022-05-23 10:11:49 +0200

    PR fixes

--- core-primitives/top-pool-author/src/api.rs
+++ core-primitives/top-pool-author/src/api.rs
@@ -58,7 +58,7 @@ impl<Block> SidechainApi<Block> {
 	fn validate_trusted_call(trusted_call_signed: TrustedCallSigned) -> ValidTransaction {
 		let from = trusted_call_signed.call.account();
 		let requires = vec![];
-		let provides = vec![from.encode()];
+		let provides = vec![(from, trusted_call_signed.nonce).encode()];
 
 		ValidTransaction { priority: 1 << 20, requires, provides, longevity: 64, propagate: true }
 	}
