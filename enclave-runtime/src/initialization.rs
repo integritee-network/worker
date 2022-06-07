aa33cdae83ecae1228e6e293201eec92c73bc39a
Gaudenz Kessler <gaudenz.kessler@scs.ch>
2022-06-07 13:01:27 +0200

    merge conflicts

--- enclave-runtime/src/initialization.rs
+++ enclave-runtime/src/initialization.rs
@@ -221,7 +221,8 @@ pub(crate) fn init_enclave_sidechain_components() -> EnclaveResult<()> {
 pub(crate) fn init_light_client(params: LightClientInitParams<Header>) -> EnclaveResult<Header> {
 	let ocall_api = GLOBAL_OCALL_API_COMPONENT.get()?;
 	let validator = itc_parentchain::light_client::io::read_or_init_validator::<Block, OcallApi>(
-		params, ocall_api,
+		params,
+		ocall_api.clone(),
 	)?;
 	let latest_header = validator.latest_finalized_header(validator.num_relays())?;
 
@@ -245,7 +246,7 @@ pub(crate) fn init_light_client(params: LightClientInitParams<Header>) -> Enclav
 
 	let stf_enclave_signer = Arc::new(EnclaveStfEnclaveSigner::new(
 		state_handler,
-		ocall_api.clone(),
+		ocall_api,
 		shielding_key_repository.clone(),
 	));
 	let indirect_calls_executor = Arc::new(IndirectCallsExecutor::new(
