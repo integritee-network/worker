e4b17ac80a06f94ad739d1c6926e45257d8234d1
echevrier <84318241+echevrier@users.noreply.github.com>
2022-06-15 19:25:47 +0200

    Fix issue#794: make `AssetId` configurable per set of extrinsics . (#795)
    Co-authored-by: echevrier <edith.chevrier@scs.ch>

--- core-primitives/extrinsics-factory/src/mock.rs
+++ core-primitives/extrinsics-factory/src/mock.rs
@@ -16,7 +16,7 @@
 */
 
 use crate::{error::Result, CreateExtrinsics};
-use itp_types::OpaqueCall;
+use itp_types::{OpaqueCall, ParentchainExtrinsicParamsBuilder};
 use sp_runtime::OpaqueExtrinsic;
 use std::vec::Vec;
 
@@ -27,7 +27,11 @@ use std::vec::Vec;
 pub struct ExtrinsicsFactoryMock;
 
 impl CreateExtrinsics for ExtrinsicsFactoryMock {
-	fn create_extrinsics(&self, _calls: &[OpaqueCall]) -> Result<Vec<OpaqueExtrinsic>> {
+	fn create_extrinsics(
+		&self,
+		_calls: &[OpaqueCall],
+		_extrinsics_params_builder: Option<ParentchainExtrinsicParamsBuilder>,
+	) -> Result<Vec<OpaqueExtrinsic>> {
 		// Intention was to map an OpaqueCall to some dummy OpaqueExtrinsic,
 		// so the output vector has the same size as the input one (and thus can be tested from the outside).
 		// However, it doesn't seem to be possible to construct an empty of dummy OpaqueExtrinsic,
