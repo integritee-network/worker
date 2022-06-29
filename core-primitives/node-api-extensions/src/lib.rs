1c30ae71bd3ab42fd70400fd2e2a214d62ef5170
Felix Müller <felix.mueller@scs.ch>
2022-06-29 15:24:38 +0200

    Make the node-api-extensions crate sgx aware

--- core-primitives/node-api-extensions/src/lib.rs
+++ core-primitives/node-api-extensions/src/lib.rs
@@ -17,20 +17,43 @@
 
 //! Some substrate-api-client extension traits.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(all(feature = "std", feature = "sgx"))]
+compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
+
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+extern crate sgx_tstd as std;
+
+// re-export module to properly feature gate sgx and regular std environment
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+pub mod sgx_reexport_prelude {
+	pub use thiserror_sgx as thiserror;
+}
+
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub use substrate_api_client::{rpc::WsRpcClient, Api, ApiClientError};
 
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub mod account;
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub mod chain;
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub mod node_api_factory;
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub mod pallet_teerex;
 
-#[cfg(feature = "mocks")]
+#[cfg(all(feature = "mocks", feature = "std"))]
 pub mod pallet_teerex_api_mock;
 
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub use account::*;
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub use chain::*;
-use itp_types::ParentchainExtrinsicParams;
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
 pub use pallet_teerex::*;
+
+use itp_types::ParentchainExtrinsicParams;
 use sp_core::sr25519;
 
 pub type ApiResult<T> = Result<T, ApiClientError>;
