0126dd4eca4450afcb8f4a33f636a1b264260176
Gaudenz Kessler <gaudenz.kessler@scs.ch>
2022-06-30 08:57:28 +0200

    use serde sgx

--- core-primitives/rpc/src/lib.rs
+++ core-primitives/rpc/src/lib.rs
@@ -1,9 +1,48 @@
-extern crate alloc;
+/*
+	Copyright 2021 Integritee AG and Supercomputing Systems AG
 
-use alloc::{borrow::ToOwned, string::String, vec::Vec};
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+*/
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(all(feature = "std", feature = "sgx"))]
+compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
+
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
+// re-export module to properly feature gate sgx and regular std environment
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+pub mod sgx_reexport_prelude {
+	pub use serde_json_sgx as serde_json;
+	pub use serde_sgx as serde;
+}
+
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+use crate::sgx_reexport_prelude::*;
+
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+use serde_derive::{Deserialize, Serialize};
+
+#[cfg(all(not(feature = "sgx"), feature = "std"))]
+use serde::{Deserialize, Serialize};
+
 use codec::{Decode, Encode};
 use itp_types::DirectRequestStatus;
-use serde::{Deserialize, Serialize};
+use std::{borrow::ToOwned, string::String, vec::Vec};
 
 #[derive(Encode, Decode, Debug)]
 pub struct RpcReturnValue {
