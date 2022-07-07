e3c755fc1b86a35a063b87111809d42e91915d0e
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2022-07-07 18:03:23 +0200

    update jsonrpc core to new version

--- core/direct-rpc-server/src/lib.rs
+++ core/direct-rpc-server/src/lib.rs
@@ -20,6 +20,9 @@
 #[cfg(all(feature = "std", feature = "sgx"))]
 compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
 
+#[cfg(all(not(feature = "std"), not(feature = "sgx")))]
+compile_error!("feature \"std\" and feature \"sgx\" cannot be disabled at the same time");
+
 #[cfg(all(not(feature = "std"), feature = "sgx"))]
 extern crate sgx_tstd as std;
 
