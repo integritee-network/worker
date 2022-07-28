b716789d895e503c7f81ae5ca25571528972800f
Sabine Proll <sabine.proll@scs.ch>
2022-07-28 13:40:22 +0200

    benchmarks run in docker-setup

--- scripts/benchmark_local-setup.sh
+++ scripts/benchmark_local-setup.sh
@@ -17,14 +17,14 @@ echo "Benchmark PID: $PID"
 sleep 40s
 
 pushd bin
-./integritee-cli -p 9930 -P 2030 trusted --direct --mrenclave $(cat ~/mrenclave.b58) benchmark 20 100 -w
+./integritee-cli -p 9930 -P 2030 trusted --direct --mrenclave "$(cat ~/mrenclave.b58)" benchmark 20 100 -w
 popd
 
 sleep 10s
 
 if test -f "./benchmark.pid"; then
     echo "Killing benchmark process"
-    kill -s SIGTERM $(cat ./benchmark.pid)
+    kill -s SIGTERM "$(cat ./benchmark.pid)"
     rm benchmark.pid
 fi
 
