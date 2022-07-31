9ef50e1f9b86306a7525cbfe528368f5af55aaeb
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-07-31 14:54:24 +0200

    [node-api] converted crate into a simple re-export crate

--- core-primitives/node-api/src/lib.rs
+++ core-primitives/node-api/src/lib.rs
@@ -15,39 +15,13 @@
 
 */
 
-//! All kind of things to talk to a node.
+//! Re-export crate for all the node-api-stuff
 
-//! Contains type definitions to talk to the node.
-///
-/// For example, you need to update this if you have a signed extensions in your node that
-/// is different from the integritee-node, or if you use the `pallet_asset_tx_payment`.
-use substrate_api_client::{
-	PlainTip, PlainTipExtrinsicParams, PlainTipExtrinsicParamsBuilder, SubstrateDefaultSignedExtra,
-	UncheckedExtrinsicV4,
-};
-
-/// Configuration for the ExtrinsicParams.
-///
-/// Valid for the default integritee node
-pub type ParentchainExtrinsicParams = PlainTipExtrinsicParams;
-pub type ParentchainExtrinsicParamsBuilder = PlainTipExtrinsicParamsBuilder;
-
-// Pay in asset fees.
-//
-// This needs to be used if the node uses the `pallet_asset_tx_payment`.
-//pub type ParentchainExtrinsicParams = AssetTipExtrinsicParams;
-//pub type ParentchainExtrinsicParamsBuilder = AssetTipExtrinsicParamsBuilder;
-
-pub type ParentchainUncheckedExtrinsic<Call> =
-	UncheckedExtrinsicV4<Call, SubstrateDefaultSignedExtra<PlainTip>>;
+#[cfg(all(feature = "std", feature = "sgx"))]
+compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
 
 #[cfg(feature = "std")]
-pub use api::*;
-
-#[cfg(feature = "std")]
-mod api {
-	use super::ParentchainExtrinsicParams;
-	use substrate_api_client::{rpc::WsRpcClient, Api};
-
-	pub type ParentchainApi = Api<sp_core::sr25519::Pair, WsRpcClient, ParentchainExtrinsicParams>;
-}
+pub use itp_api_client_extensions::*;
+pub use itp_api_client_types::*;
+pub use itp_node_api_metadata::*;
+pub use itp_node_api_metadata_provider::*;
