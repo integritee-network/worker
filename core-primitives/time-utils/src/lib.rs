b9b5908f29dfa9a616455bb4032b96d5f6c73ee1
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2022-08-03 13:05:07 +0200

    add note comment

--- core-primitives/time-utils/src/lib.rs
+++ core-primitives/time-utils/src/lib.rs
@@ -47,7 +47,9 @@ pub fn duration_difference(from: Duration, to: Duration) -> Option<Duration> {
 	to.checked_sub(from)
 }
 
-/// Returns current duration since unix epoch.
+/// Returns current duration since unix epoch with SystemTime::now().
+/// Note: subsequent calls are not guaranteed to be monotonic.
+/// (https://doc.rust-lang.org/std/time/struct.SystemTime.html)
 pub fn duration_now() -> Duration {
 	let now = SystemTime::now();
 	now.duration_since(SystemTime::UNIX_EPOCH).unwrap_or_else(|e| {
