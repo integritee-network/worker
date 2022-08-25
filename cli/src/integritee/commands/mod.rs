50663b37e360edbc821c0e9adee0bf388e046aab
Gaudenz Kessler <gaudenz.kessler@scs.ch>
2022-08-25 22:32:01 +0200

    Refactor cli commands

---
+++ cli/src/integritee/commands/mod.rs
@@ -0,0 +1,6 @@
+pub mod balance;
+pub mod faucet;
+pub mod integritee_command_utils;
+pub mod listen;
+pub mod shield_funds;
+pub mod transfer;
---
+++ cli/src/integritee/mod.rs
@@ -0,0 +1,142 @@
+/*
+	Copyright 2021 Integritee AG and Supercomputing Systems AG
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+*/
+
+use crate::{
+	command_utils::*,
+	integritee::commands::{
+		balance::BalanceCommands, faucet::FaucetCommands, listen::ListenCommands,
+		shield_funds::ShieldFundsCommands, transfer::TransferCommands,
+	},
+	Cli,
+};
+use base58::ToBase58;
+use chrono::{DateTime, Utc};
+use clap::Subcommand;
+use itc_rpc_client::direct_client::DirectApi;
+use itp_node_api::api_client::PalletTeerexApi;
+use sp_application_crypto::{ed25519, sr25519};
+use sp_core::{crypto::Ss58Codec, Pair};
+use std::{
+	path::PathBuf,
+	time::{Duration, UNIX_EPOCH},
+};
+use substrate_api_client::Metadata;
+use substrate_client_keystore::{KeystoreExt, LocalKeystore};
+
+mod commands;
+
+#[derive(Subcommand)]
+pub enum IntegriteeCommands {
+	/// query parentchain balance for AccountId
+	Balance(BalanceCommands),
+
+	/// generates a new account for the integritee chain in your local keystore
+	NewAccount,
+
+	/// lists all accounts in your local keystore for the integritee chain
+	ListAccounts,
+
+	/// query node metadata and print it as json to stdout
+	PrintMetadata,
+
+	/// query sgx-runtime metadata and print it as json to stdout
+	PrintSgxMetadata,
+
+	/// send some bootstrapping funds to supplied account(s)
+	Faucet(FaucetCommands),
+
+	/// transfer funds from one parentchain account to another
+	Transfer(TransferCommands),
+
+	/// query enclave registry and list all workers
+	ListWorkers,
+
+	/// listen to parentchain events
+	Listen(ListenCommands),
+
+	/// Transfer funds from an parentchain account to an incognito account
+	ShieldFunds(ShieldFundsCommands),
+}
+
+impl IntegriteeCommands {
+	pub fn run(&self, cli: &Cli) {
+		match self {
+			IntegriteeCommands::Balance(cmd) => cmd.run(cli),
+			IntegriteeCommands::NewAccount => new_account(),
+			IntegriteeCommands::ListAccounts => list_accounts(),
+			IntegriteeCommands::PrintMetadata => print_metadata(cli),
+			IntegriteeCommands::PrintSgxMetadata => print_sgx_metadata(cli),
+			IntegriteeCommands::Faucet(cmd) => cmd.run(cli),
+			IntegriteeCommands::Transfer(cmd) => cmd.run(cli),
+			IntegriteeCommands::ListWorkers => list_workers(cli),
+			IntegriteeCommands::Listen(cmd) => cmd.run(cli),
+			IntegriteeCommands::ShieldFunds(cmd) => cmd.run(cli),
+		}
+	}
+}
+
+fn new_account() {
+	let store = LocalKeystore::open(PathBuf::from(&KEYSTORE_PATH), None).unwrap();
+	let key: sr25519::AppPair = store.generate().unwrap();
+	drop(store);
+	println!("{}", key.public().to_ss58check());
+}
+
+fn list_accounts() {
+	let store = LocalKeystore::open(PathBuf::from(&KEYSTORE_PATH), None).unwrap();
+	println!("sr25519 keys:");
+	for pubkey in store.public_keys::<sr25519::AppPublic>().unwrap().into_iter() {
+		println!("{}", pubkey.to_ss58check());
+	}
+	println!("ed25519 keys:");
+	for pubkey in store.public_keys::<ed25519::AppPublic>().unwrap().into_iter() {
+		println!("{}", pubkey.to_ss58check());
+	}
+	drop(store);
+}
+
+fn print_metadata(cli: &Cli) {
+	let meta = get_chain_api(cli).get_metadata().unwrap();
+	println!("Metadata:\n {}", Metadata::pretty_format(&meta).unwrap());
+}
+
+fn print_sgx_metadata(cli: &Cli) {
+	let worker_api_direct = get_worker_api_direct(cli);
+	let metadata = worker_api_direct.get_state_metadata().unwrap();
+	println!("Metadata:\n {}", Metadata::pretty_format(&metadata).unwrap());
+}
+
+fn list_workers(cli: &Cli) {
+	let api = get_chain_api(cli);
+	let wcount = api.enclave_count(None).unwrap();
+	println!("number of workers registered: {}", wcount);
+	for w in 1..=wcount {
+		let enclave = api.enclave(w, None).unwrap();
+		if enclave.is_none() {
+			println!("error reading enclave data");
+			continue
+		};
+		let enclave = enclave.unwrap();
+		let timestamp =
+			DateTime::<Utc>::from(UNIX_EPOCH + Duration::from_millis(enclave.timestamp as u64));
+		println!("Enclave {}", w);
+		println!("   AccountId: {}", enclave.pubkey.to_ss58check());
+		println!("   MRENCLAVE: {}", enclave.mr_enclave.to_base58());
+		println!("   RA timestamp: {}", timestamp);
+		println!("   URL: {}", enclave.url);
+	}
+}
