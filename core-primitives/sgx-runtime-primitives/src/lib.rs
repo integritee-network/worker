8495d06dcaf02449939ba1ce6e169965ebf0e5f8
orion <szilard.parrag@gmail.com>
2022-10-07 13:58:33 +0200

    fixup! fixup! core-primitives: add new crate sgx-runtime-primitives

--- core-primitives/sgx-runtime-primitives/src/lib.rs
+++ core-primitives/sgx-runtime-primitives/src/lib.rs
@@ -14,5 +14,12 @@
 	limitations under the License.
 
 */
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(all(feature = "std", feature = "sgx"))]
+compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
+
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+extern crate sgx_tstd as std;
 pub mod constants;
 pub mod types;
