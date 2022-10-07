338f4f33f554cba5254792e277d1e924f61551fb
orion <szilard.parrag@gmail.com>
2022-10-07 16:19:14 +0200

    remove sgx attributes

--- core-primitives/sgx-runtime-primitives/src/lib.rs
+++ core-primitives/sgx-runtime-primitives/src/lib.rs
@@ -16,10 +16,5 @@
 */
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(all(feature = "std", feature = "sgx"))]
-compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the same time");
-
-#[cfg(all(not(feature = "std"), feature = "sgx"))]
-extern crate sgx_tstd as std;
 pub mod constants;
 pub mod types;
