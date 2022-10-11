cac65b55044ea1f0cd8e34c086163ef92398c46a
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2022-10-11 14:36:10 +0200

    cargo fmt

--- enclave-runtime/src/initialization.rs
+++ enclave-runtime/src/initialization.rs
@@ -188,8 +188,7 @@ pub(crate) fn init_enclave(mu_ra_url: String, untrusted_worker_url: String) -> E
 	let sidechain_block_import_queue = Arc::new(EnclaveSidechainBlockImportQueue::default());
 	GLOBAL_SIDECHAIN_IMPORT_QUEUE_COMPONENT.initialize(sidechain_block_import_queue);
 
-	let attestation_handler =
-		Arc::new(IasAttestationHandler::new(ocall_api));
+	let attestation_handler = Arc::new(IasAttestationHandler::new(ocall_api));
 	GLOBAL_ATTESTATION_HANDLER_COMPONENT.initialize(attestation_handler);
 
 	Ok(())
