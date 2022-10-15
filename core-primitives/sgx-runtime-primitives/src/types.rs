f97accea2253c6bc914240aa5ed0b2a1ca089dba
Szilard Parrag <szilard.parrag@gmail.com>
2022-10-15 14:34:00 +0200

    sgx-runtime-primitives: move SideChainBlockNumber, SideChainTimeStamp

--- core-primitives/sgx-runtime-primitives/src/types.rs
+++ core-primitives/sgx-runtime-primitives/src/types.rs
@@ -28,6 +28,8 @@ pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
 
 /// An index to a block.
 pub type BlockNumber = u32;
+pub type SidechainBlockNumber = u64;
+pub type SidechainTimestamp = u64;
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
 pub type Signature = MultiSignature;
