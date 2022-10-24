c987eb65088ef8f891d0aefc064a3ae02cf74dc2
orion <szilard.parrag@gmail.com>
2022-10-24 10:56:32 +0200

    update rust sgx sdk to v1.1.6-testing, add exact commit hash to script

--- UpdateRustSGXSDK.mk
+++ UpdateRustSGXSDK.mk
@@ -10,6 +10,8 @@ VERSION_FILE = rust-sgx-sdk/version
 LOCAL_VERSION = $(shell cat $(VERSION_FILE))
 COMMAND = git ls-remote $(REPO) HEAD | awk '{ print $$1 }'
 REMOTE_VERSION = $(shell $(COMMAND))
+# or specify the exact hash if you need a non-default branch / tag / commit etc.
+#REMOTE_VERSION = 9c1bbd52f188f600a212b57c916124245da1b7fd
 
 # update the SDK files
 all: updatesdk
@@ -22,6 +24,7 @@ ifneq ('$(LOCAL_VERSION)','$(REMOTE_VERSION)')
 
 	@rm -rf $(SDK_PATH_GIT)
 	@$(GIT) clone $(REPO) $(SDK_PATH_GIT)
+	@$(GIT) -C  $(SDK_PATH_GIT) checkout $(REMOTE_VERSION)
 	rsync -a $(SDK_PATH_GIT)/edl $(SDK_PATH)
 	rsync -a $(SDK_PATH_GIT)/common $(SDK_PATH)
 	rm -rf $(SDK_PATH_GIT)
