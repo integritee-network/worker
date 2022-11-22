3bebf8bab913cd4b2b70f25dd67486ed34ffebaa
Szilárd Parrag <szilard.parrag@gmail.com>
2022-11-22 15:29:40 +0100

    Minor itc-light-client cleanup  (#1101)
    * light-client: rename route to ancestors
    
    * ligh-client: use local variable in decode_and_verify_finalizes
    
    * light-client: verify_grandpa_proof does not the whole Justification, only the encoded part is needed from the tuple
    
    * light-client: use variable instead of its value
    
    * light-client: simplify loop
    
    * light-client: use .extend(self, Iter) instead of for loop
    
    * light-client: replace match with if let
    
    * light-client: inline error msg &str
    
    * light-client: use std::mem::replace for setting new block_header
    
    * light-client: remove placeholder variable
    
    * light-client: use iter.find() instead of loop
    
    * light-client: extract common get_tracked_relay() operation
    
    * light-client: refactor verify_voter_set
    
    * light-client: use if let in fn ancestry() instead of match

--- core/parentchain/light-client/src/finality.rs
+++ core/parentchain/light-client/src/finality.rs
@@ -31,7 +31,7 @@ use sp_finality_grandpa::{AuthorityId, ScheduledChange, GRANDPA_ENGINE_ID};
 use sp_runtime::{
 	generic::Digest,
 	traits::{Block as ParentchainBlockTrait, Header as HeaderTrait},
-	Justification, Justifications,
+	EncodedJustification, Justifications,
 };
 
 #[derive(Default)]
@@ -96,7 +96,7 @@ where
 		match grandpa_justification {
 			Some(justification) => {
 				if let Err(err) = Self::verify_grandpa_proof::<Block>(
-					(GRANDPA_ENGINE_ID, justification),
+					justification,
 					block_hash,
 					block_num,
 					validator_set_id,
@@ -105,7 +105,7 @@ where
 					// FIXME: Printing error upon invalid justification, but this will need a better fix
 					// see issue #353
 					error!("Block {:?} contained invalid justification: {:?}", block_num, err);
-					relay.unjustified_headers.push(header.hash());
+					relay.unjustified_headers.push(block_hash);
 					relay.set_last_finalized_block_header(header);
 					return Err(err)
 				}
@@ -114,7 +114,7 @@ where
 				Ok(())
 			},
 			None => {
-				relay.unjustified_headers.push(header.hash());
+				relay.unjustified_headers.push(block_hash);
 				relay.set_last_finalized_block_header(header);
 
 				debug!(
@@ -159,7 +159,7 @@ impl GrandpaFinality {
 	}
 
 	fn verify_grandpa_proof<Block: ParentchainBlockTrait>(
-		justification: Justification,
+		encoded_justification: EncodedJustification,
 		hash: Block::Hash,
 		number: NumberFor<Block>,
 		set_id: u64,
@@ -170,7 +170,7 @@ impl GrandpaFinality {
 	{
 		// We don't really care about the justification, as long as it's valid
 		let _ = GrandpaJustification::<Block>::decode_and_verify_finalizes(
-			&justification.1,
+			&encoded_justification,
 			(hash, number),
 			set_id,
 			voters,
