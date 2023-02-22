886f2df7f157f31965a6d948677559ed28f8c14e
Andrew Burger <andrewburger1991@gmail.com>
2023-02-22 11:11:08 +0000

    Adding code to allow for calling find LCA

--- sidechain/consensus/common/src/is_descendent_of_builder.rs
+++ sidechain/consensus/common/src/is_descendent_of_builder.rs
@@ -12,36 +12,36 @@ use std::borrow::Borrow;
 
 // TODO: Build Cache for Latest Blocks
 
-// // Normally implemented on the Client in substrate I believe?
-// pub trait HeaderDbTrait<Block: BlockT> {
-//     /// Retrieves Header for the corresponding block hash
-//     // fn header(&self, hash: H256) -> Option<Block::HeaderType>;
-//     fn header(&self, hash: H256) -> Option<SidechainHeader>;
-// }
+// Normally implemented on the Client in substrate I believe?
+pub trait HeaderDbTrait {
+    type Header;
+    /// Retrieves Header for the corresponding block hash
+    // fn header(&self, hash: H256) -> Option<Block::HeaderType>;
+    fn header(&self, hash: &H256) -> Option<Self::Header>;
+}
 
 pub struct HeaderDb<Hash, Header>(HashMap<Hash, Header>);
-// impl<Hash: PartialEq + HashT + Into<H256>, Header> HeaderDbTrait<Block> for HeaderDb<Hash, Header>
-// where
-//     Header: HeaderT,
-// {
-//     fn header(&self, hash: Hash) -> Option<SidechainHeader> {
-//         self.0.get(&hash.into())     
-//     }
-// }
-impl<Hash: PartialEq + Eq +  HashT + Clone, Header: Clone> HeaderDb<Hash, Header> {
-    fn new() -> Self {
+impl<Hash: PartialEq + HashT + Into<H256> + From<H256> + std::cmp::Eq + Clone, Header> HeaderDbTrait for HeaderDb<Hash, Header>
+where
+    Header: HeaderT + Clone + Into<SidechainHeader>
+{
+    type Header = SidechainHeader;
+    fn header(&self, hash: &H256) -> Option<Self::Header> {
+        let header = self.0.get(&Hash::from(*hash))?;
+        Some(header.clone().into())
+    }
+}
+impl<Hash: PartialEq + Eq + HashT + Clone, Header: Clone> HeaderDb<Hash, Header> {
+    pub fn new() -> Self {
         Self {
             0: HashMap::new(),
         }
-    }
-    fn header(&self, hash: Hash) -> Option<Header> {
-        self.0.get(&hash).cloned()
     }
 }
 
 // TODO: Pass in Block as Generic param?
 struct IsDescendentOfBuilder<Hash, HeaderDb>(PhantomData<(Hash, HeaderDb)>);
-impl<'a, Hash: PartialEq + std::hash::Hash + Default, HeaderDb> IsDescendentOfBuilder<Hash, HeaderDb> {
+impl<'a, Hash: PartialEq + HashT + Default + Into<H256> + From<H256> + Clone, HeaderDb: HeaderDbTrait> IsDescendentOfBuilder<Hash, HeaderDb> {
     fn build_is_descendent_of(
         current: Option<(&'a Hash, &'a Hash)>,
         header_db: &'a HeaderDb,
@@ -71,28 +71,30 @@ impl<'a, Hash: PartialEq + std::hash::Hash + Default, HeaderDb> IsDescendentOfBu
             }
 
             let ancestor = 
-                <LowestCommonAncestorFinder<Hash, HeaderDb>>::find_lowest_common_ancestor(head, base, header_db);
+                <LowestCommonAncestorFinder<Hash, HeaderDb>>::find_lowest_common_ancestor(head, base, header_db)?;
             Ok(ancestor == *base)
         }
     }
 }
 
 struct LowestCommonAncestorFinder<Hash, HeaderDb>(PhantomData<(Hash, HeaderDb)>);
-impl <Hash: PartialEq + Default, HeaderDb> LowestCommonAncestorFinder<Hash, HeaderDb> {
-    fn find_lowest_common_ancestor(a: &Hash, b: &Hash, header_db: &HeaderDb) -> Hash {
+impl <Hash: PartialEq + Default + Into<H256> + From<H256> + Clone, HeaderDb: HeaderDbTrait> LowestCommonAncestorFinder<Hash, HeaderDb> {
+    fn find_lowest_common_ancestor(a: &Hash, b: &Hash, header_db: &HeaderDb) -> Result<Hash, ()> {
+        let header_1 = header_db.header(&<Hash as Into<H256>>::into(a.clone())).ok_or(())?;
         // TODO: Implement lowest common ancestor algorithm
         /* 
         ** Need to access blocks and their headers for BlockHash and BlockNumber perhaps a cache?
         ** (BlockHash -> BlockHeader)
         */ 
-        Default::default()
+        Ok(Default::default())
     }
 }
 
 #[test]
 fn test_build_is_descendent_of_works() {
-    let db = <HeaderDb<u64, SidechainHeader>>::new();
-    let is_descendent_of = <IsDescendentOfBuilder<u64, HeaderDb<u64, SidechainHeader>>>::build_is_descendent_of(None, &db);
-    let my_result = is_descendent_of(&42u64, &43u64);
-    assert_eq!(my_result, Ok(false));
+    let db = <HeaderDb<H256, SidechainHeader>>::new();
+    let is_descendent_of = <IsDescendentOfBuilder<H256, HeaderDb<H256, SidechainHeader>>>::build_is_descendent_of(None, &db);
+    let my_result = is_descendent_of(&[1; 32].into(), &[0; 32].into());
+    // TODO: Add something there Nothing in DB yet
+    assert_eq!(my_result, Err(()));
 }
