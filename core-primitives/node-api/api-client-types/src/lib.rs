d1762bdd55cb47e6653bb40a78901437bd1f7492
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-04-05 14:23:43 +0200

    [itp-api-client-types] re-export extrinsic params trait.

--- core-primitives/node-api/api-client-types/src/lib.rs
+++ core-primitives/node-api/api-client-types/src/lib.rs
@@ -26,8 +26,8 @@ pub use itp_types::parentchain::{
 	AccountId, Address, Balance, Hash, Index, Signature as PairSignature,
 };
 pub use substrate_api_client::{
-	AssetTip, CallIndex, GenericAdditionalParams, GenericExtrinsicParams, GenericSignedExtra,
-	PlainTip, StaticExtrinsicSigner, UncheckedExtrinsicV4,
+	AssetTip, CallIndex, ExtrinsicParams, GenericAdditionalParams, GenericExtrinsicParams,
+	GenericSignedExtra, PlainTip, StaticExtrinsicSigner, UncheckedExtrinsicV4,
 };
 
 pub type ParentchainPlainTip = PlainTip<Balance>;
