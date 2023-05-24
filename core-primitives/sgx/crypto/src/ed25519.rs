19b873e4119e13a5ed37932a2dda85a6b8646f60
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-05-24 08:32:38 +0200

    [itp-sgx-crypto] put some functions behind a trait.

--- core-primitives/sgx/crypto/src/ed25519.rs
+++ core-primitives/sgx/crypto/src/ed25519.rs
@@ -15,42 +15,92 @@
 
 */
 
-use derive_more::Display;
-
-#[derive(Copy, Clone, Debug, Display)]
-pub struct Ed25519Seal;
+use crate::error::Result;
+use sp_core::ed25519;
 
 #[cfg(feature = "sgx")]
 pub use sgx::*;
 
+pub trait Ed25519Sealing {
+	fn unseal_pubkey(&self) -> Result<ed25519::Public>;
+
+	fn unseal_pair(&self) -> Result<ed25519::Pair>;
+
+	fn exists(&self) -> bool;
+
+	fn create_sealed_if_absent(&self) -> Result<()>;
+
+	fn create_sealed(&self) -> Result<()>;
+}
+
 #[cfg(feature = "sgx")]
 pub mod sgx {
-
-	use super::*;
-	use crate::error::{Error, Result};
+	use crate::{
+		error::{Error, Result},
+		key_repository::KeyRepository,
+		Ed25519Sealing,
+	};
 	use codec::Encode;
 	use itp_settings::files::SEALED_SIGNER_SEED_FILE;
-	use itp_sgx_io::{seal, unseal, SealedIO, StaticSealedIO};
+	use itp_sgx_io::{seal, unseal, SealedIO};
 	use log::*;
 	use sgx_rand::{Rng, StdRng};
 	use sp_core::{crypto::Pair, ed25519};
-	use std::{path::Path, sgxfs::SgxFile};
+	use std::{path::PathBuf, sgxfs::SgxFile};
 
-	impl StaticSealedIO for Ed25519Seal {
-		type Error = Error;
-		type Unsealed = ed25519::Pair;
+	/// Gets a repository for an Ed25519 keypair and initializes
+	/// a fresh key pair if it doesn't exist at `path`.
+	pub fn get_ed25519_repository(
+		path: PathBuf,
+	) -> Result<KeyRepository<ed25519::Pair, Ed25519Seal>> {
+		let ed25519_seal = Ed25519Seal::new(path);
+		ed25519_seal.create_sealed_if_absent()?;
+		let signing_pair = ed25519_seal.unseal_pair()?;
+		Ok(KeyRepository::new(signing_pair, ed25519_seal.into()))
+	}
 
-		fn unseal_from_static_file() -> Result<ed25519::Pair> {
-			let raw = unseal(SEALED_SIGNER_SEED_FILE)?;
+	#[derive(Clone, Debug)]
+	pub struct Ed25519Seal {
+		base_path: PathBuf,
+	}
 
-			let key = ed25519::Pair::from_seed_slice(&raw)
-				.map_err(|e| Error::Other(format!("{:?}", e).into()))?;
-
-			Ok(key.into())
+	impl Ed25519Seal {
+		pub fn new(base_path: PathBuf) -> Self {
+			Self { base_path }
 		}
 
-		fn seal_to_static_file(unsealed: &Self::Unsealed) -> Result<()> {
-			Ok(unsealed.seed().using_encoded(|bytes| seal(bytes, SEALED_SIGNER_SEED_FILE))?)
+		pub fn path(&self) -> PathBuf {
+			self.base_path.join(SEALED_SIGNER_SEED_FILE)
+		}
+	}
+
+	impl Ed25519Sealing for Ed25519Seal {
+		fn unseal_pubkey(&self) -> Result<ed25519::Public> {
+			self.unseal().map(Into::into)
+		}
+
+		fn unseal_pair(&self) -> Result<ed25519::Pair> {
+			self.unseal()
+		}
+
+		fn exists(&self) -> bool {
+			SgxFile::open(self.path()).is_ok()
+		}
+
+		fn create_sealed_if_absent(&self) -> Result<()> {
+			if !self.exists() {
+				info!("Keyfile not found, creating new! {}", self.path().display());
+				return self.create_sealed()
+			}
+			Ok(())
+		}
+
+		fn create_sealed(&self) -> Result<()> {
+			let mut seed = [0u8; 32];
+			let mut rand = StdRng::new()?;
+			rand.fill_bytes(&mut seed);
+
+			Ok(seal(&seed, self.path())?)
 		}
 	}
 
@@ -59,30 +109,14 @@ pub mod sgx {
 		type Unsealed = ed25519::Pair;
 
 		fn unseal(&self) -> Result<Self::Unsealed> {
-			Self::unseal_from_static_file()
+			let raw = unseal(self.path())?;
+
+			ed25519::Pair::from_seed_slice(&raw)
+				.map_err(|e| Error::Other(format!("{:?}", e).into()))
 		}
 
 		fn seal(&self, unsealed: &Self::Unsealed) -> Result<()> {
-			Self::seal_to_static_file(unsealed)
+			Ok(unsealed.seed().using_encoded(|bytes| seal(bytes, self.path()))?)
 		}
-	}
-
-	pub fn create_sealed_if_absent() -> Result<()> {
-		if SgxFile::open(SEALED_SIGNER_SEED_FILE).is_err() {
-			if Path::new(SEALED_SIGNER_SEED_FILE).exists() {
-				panic!("[Enclave] Keyfile {} exists but can't be opened. has it been written by the same enclave?", SEALED_SIGNER_SEED_FILE);
-			}
-			info!("[Enclave] Keyfile not found, creating new! {}", SEALED_SIGNER_SEED_FILE);
-			return create_sealed_seed()
-		}
-		Ok(())
-	}
-
-	pub fn create_sealed_seed() -> Result<()> {
-		let mut seed = [0u8; 32];
-		let mut rand = StdRng::new()?;
-		rand.fill_bytes(&mut seed);
-
-		Ok(seal(&seed, SEALED_SIGNER_SEED_FILE)?)
 	}
 }
