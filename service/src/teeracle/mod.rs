0370f5dbcc5fa6aa5c9a2c2e753405a53c3e4521
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-06-06 16:24:01 +0200

    [integritee-service/teeracle] minor code simplifications by introducing results

--- service/src/teeracle/mod.rs
+++ service/src/teeracle/mod.rs
@@ -15,7 +15,10 @@
 
 */
 
-use crate::teeracle::interval_scheduling::schedule_on_repeating_intervals;
+use crate::{
+	error::{Error, ServiceResult},
+	teeracle::interval_scheduling::schedule_on_repeating_intervals,
+};
 use codec::{Decode, Encode};
 use itp_enclave_api::teeracle_api::TeeracleApi;
 use itp_node_api::api_client::ParentchainApi;
@@ -39,7 +42,10 @@ pub(crate) fn start_interval_market_update<E: TeeracleApi>(
 	tokio_handle: &Handle,
 ) {
 	let updates_to_run = || {
-		execute_market_update(api, enclave_api, tokio_handle);
+		if let Err(e) = execute_market_update(api, enclave_api, tokio_handle) {
+			error!("Error running teeracle update {:?}", e)
+		}
+
 		// TODO: Refactor and add this back according to ISSUE: https://github.com/integritee-network/worker/issues/1300
 		// execute_weather_update(api, enclave_api, tokio_handle);
 	};
@@ -55,23 +61,13 @@ fn execute_weather_update<E: TeeracleApi>(
 	node_api: &ParentchainApi,
 	enclave: &E,
 	tokio_handle: &Handle,
-) {
-	let updated_extrinsic = match enclave.update_weather_data_xt("54.32", "15.37") {
-		Err(e) => {
-			error!("{:?}", e);
-			increment_number_of_request_failures();
-			return
-		},
-		Ok(r) => r,
-	};
+) -> ServiceResult<()> {
+	let updated_extrinsic = enclave.update_weather_data_xt("54.32", "15.37").map_err(|e| {
+		increment_number_of_request_failures();
+		Error::Custom(format!("{:?}", e).into())
+	})?;
 
-	let extrinsics = match <Vec<OpaqueExtrinsic>>::decode(&mut updated_extrinsic.as_slice()) {
-		Ok(calls) => calls,
-		Err(e) => {
-			error!("Failed to decode opaque extrinsics(s): {:?}: ", e);
-			return
-		},
-	};
+	let extrinsics = <Vec<OpaqueExtrinsic>>::decode(&mut updated_extrinsic.as_slice())?;
 
 	extrinsics.into_iter().for_each(|call| {
 		let node_api_clone = node_api.clone();
@@ -96,30 +92,22 @@ fn execute_weather_update<E: TeeracleApi>(
 			println!("[<] Extrinsic got included into a block. Hash: {:?}\n", extrinsic_hash);
 		});
 	});
+
+	Ok(())
 }
 
 fn execute_market_update<E: TeeracleApi>(
 	node_api: &ParentchainApi,
 	enclave: &E,
 	tokio_handle: &Handle,
-) {
+) -> ServiceResult<()> {
 	// Get market data for usd (hardcoded)
-	let updated_extrinsic = match enclave.update_market_data_xt("TEER", "USD") {
-		Err(e) => {
-			error!("{:?}", e);
-			increment_number_of_request_failures();
-			return
-		},
-		Ok(r) => r,
-	};
+	let updated_extrinsic = enclave.update_market_data_xt("TEER", "USD").map_err(|e| {
+		increment_number_of_request_failures();
+		Error::Custom(format!("{:?}", e).into())
+	})?;
 
-	let extrinsics: Vec<OpaqueExtrinsic> = match Decode::decode(&mut updated_extrinsic.as_slice()) {
-		Ok(calls) => calls,
-		Err(e) => {
-			error!("Failed to decode opaque extrinsic(s): {:?}: ", e);
-			return
-		},
-	};
+	let extrinsics = <Vec<OpaqueExtrinsic>>::decode(&mut updated_extrinsic.as_slice())?;
 
 	// Send the extrinsics to the parentchain and wait for InBlock confirmation.
 	for call in extrinsics.into_iter() {
@@ -147,4 +135,6 @@ fn execute_market_update<E: TeeracleApi>(
 			println!("[<] Extrinsic got included into a block. Hash: {:?}\n", extrinsic_hash);
 		});
 	}
+
+	Ok(())
 }
