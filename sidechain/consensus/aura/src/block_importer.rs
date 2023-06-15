93ea7dfe5a581dd9ec5980b76a6dffb397941eb5
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-06-15 16:45:27 +0200

    [its-consensus-aura/block_importer] add some more logs

--- sidechain/consensus/aura/src/block_importer.rs
+++ sidechain/consensus/aura/src/block_importer.rs
@@ -276,6 +276,19 @@ impl<
 		sidechain_block: &SignedSidechainBlock::Block,
 		last_imported_parentchain_header: &ParentchainBlock::Header,
 	) -> Result<ParentchainBlock::Header, ConsensusError> {
+		let last = last_imported_parentchain_header;
+		debug!("Peeking parentchain header");
+		debug!(
+			"sidechain block parentchain head: {}",
+			sidechain_block.block_data().layer_one_head()
+		);
+		debug!(
+			"last imported head: {}, number: {:?}, parenthash: {}",
+			last.hash(),
+			last.number(),
+			last.parent_hash()
+		);
+
 		let parentchain_header_hash_to_peek = sidechain_block.block_data().layer_one_head();
 		if parentchain_header_hash_to_peek == last_imported_parentchain_header.hash() {
 			debug!("No queue peek necessary, sidechain block references latest imported parentchain block");
