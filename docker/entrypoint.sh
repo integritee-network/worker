9751046de89f62031562dcfba6f1ae7d938a3e05
Alain Brenzikofer <alain@integritee.network>
2023-07-18 11:58:32 +0200

    add debug log in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 # run aesmd in the background
 /opt/intel/sgx-aesm-service/aesm/aesm_service
 
+# for debugging: will be in the CI logs:
+cat /etc/sgx_default_qcnl.conf
+
 exec /usr/local/bin/integritee-service "${@}"
