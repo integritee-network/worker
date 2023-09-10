da3185cb205598891916f124b7acdecd9e27a571
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-09-10 13:29:47 +0200

    [local-setup] adjust log levels

--- local-setup/py/worker.py
+++ local-setup/py/worker.py
@@ -162,7 +162,8 @@ class Worker:
                                         'itp_stf_state_handler=debug,'
                                         'its_consensus_common=debug,'
                                         'its_consensus_aura=trace,'
-                                        'itc_parentchain_indirect_calls_executor=trace,'
+                                        'itc_parentchain_indirect_calls_executor=info,'
+                                        'itc_parentchain_light_client=info,'
                                         'itc_parentchain_block_importer=debug,'
                                         'ita_stf=debug')
         worker_cmd = self._assemble_cmd(flags=flags, subcommand_flags=subcommand_flags)
