ac3251e823ec2bafb6b9b5cc6f999f7aff1ddb2a
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-09-14 16:52:39 +0200

    [node-metadata] fix deprecated methods

--- core-primitives/node-api/metadata/src/lib.rs
+++ core-primitives/node-api/metadata/src/lib.rs
@@ -94,13 +94,12 @@ impl NodeMetadata {
 	) -> Result<[u8; 2]> {
 		let pallet = match &self.node_metadata {
 			None => return Err(Error::MetadataNotSet),
-			Some(m) => m.pallet(pallet_name).map_err(Error::NodeMetadata)?,
+			Some(m) => m.pallet_by_name_err(pallet_name)?,
 		};
 		let call_index = pallet
-			.call_indexes
-			.get(call_name)
+			.call_variant_by_name(call_name)
 			.ok_or_else(|| Error::NodeMetadata(MetadataError::CallNotFound(call_name)))?;
-		Ok([pallet.index, *call_index])
+		Ok([pallet.index(), call_index.index])
 	}
 
 	/// Generic storages:
