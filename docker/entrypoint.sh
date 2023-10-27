9a3b494195166666a67dabd8c918e398113bc5da
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-10-27 21:25:35 +0900

    [docker] extract the hex value of the mrenclave in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,10 @@ set -e
 # Check if the first argument is "mrenclave"
 if [ "$1" = "mrenclave" ]; then
     # If "mrenclave" is provided, execute the corresponding command
-    $SGX_ENCLAVE_SIGNER dump -enclave /usr/local/bin/enclave.signed.so -dumpfile df.out && \
-        /usr/local/bin/extract_identity < df.out && rm df.out
+    $SGX_ENCLAVE_SIGNER dump \
+      -enclave /usr/local/bin/enclave.signed.so \
+      -dumpfile df.out && \
+        /usr/local/bin/extract_identity < df.out && rm df.out | grep -o -E '[0-9a-fA-F]{64}'
 
 else
     # If no specific command is provided, execute the default unnamed command
