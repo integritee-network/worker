10abe91e678cb72e78f08a8c02460285ab43fab2
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-10-27 20:53:51 +0900

    [docker] fix printing the mrenclave

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$1" = "mrenclave" ]; then
     exec "${SGX_ENCLAVE_SIGNER}" dump \
       -enclave /usr/local/bin/enclave.signed.so \
       -dumpfile df.out && \
-      exec /usr/local/bin/extract_identity < df.out && rm df.out
+      /usr/local/bin/extract_identity < df.out && rm df.out
 
 else
     # If no specific command is provided, execute the default unnamed command
