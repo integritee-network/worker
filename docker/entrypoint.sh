d4a362b20a97dc3165eab5bd5c975f34ed0257e3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-10-27 20:43:52 +0900

    [docker] include sgx_sign utility in worker image and add `mrenclave` command to the `entry_point.sh`

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
-# run aesmd in the background
-/opt/intel/sgx-aesm-service/aesm/aesm_service
+# Check if the first argument is "mrenclave"
+if [ "$1" = "mrenclave" ]; then
+    # If "mrenclave" is provided, execute the corresponding command
+    exec "${SGX_ENCLAVE_SIGNER}" dump \
+      -enclave /usr/local/bin/enclave.signed.so \
+      -dumpfile df.out && \
+      exec /usr/local/bin/extract_identity < df.out && rm df.out
 
-exec /usr/local/bin/integritee-service "${@}"
+else
+    # If no specific command is provided, execute the default unnamed command
+
+    # run aesmd in the background
+    /opt/intel/sgx-aesm-service/aesm/aesm_service
+
+    exec /usr/local/bin/integritee-service "${@}"
+fi
