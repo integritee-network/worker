b18ce390cdfa03f07704fde445e170275d62f268
Alain Brenzikofer <alain@integritee.network>
2023-11-17 10:34:53 +0100

    clean up rpc api definitions

--- enclave-runtime/src/rpc/worker_api_direct.rs
+++ enclave-runtime/src/rpc/worker_api_direct.rs
@@ -64,14 +64,10 @@ where
 	GetterExecutor: ExecuteGetter + Send + Sync + 'static,
 	AccessShieldingKey: AccessPubkey<KeyType = Rsa3072PubKey> + Send + Sync + 'static,
 {
-	let io = IoHandler::new();
+	let mut io =
+		direct_top_pool_api::add_top_pool_direct_rpc_methods(top_pool_author, IoHandler::new());
 
-	// Add direct TOP pool rpc methods
-	let mut io = direct_top_pool_api::add_top_pool_direct_rpc_methods(top_pool_author, io);
-
-	// author_getShieldingKey
-	let rsa_pubkey_name: &str = "author_getShieldingKey";
-	io.add_sync_method(rsa_pubkey_name, move |_: Params| {
+	io.add_sync_method("author_getShieldingKey", move |_: Params| {
 		let rsa_pubkey = match shielding_key.retrieve_pubkey() {
 			Ok(key) => key,
 			Err(status) => {
@@ -93,8 +89,7 @@ where
 		Ok(json!(json_value.to_hex()))
 	});
 
-	let mu_ra_url_name: &str = "author_getMuRaUrl";
-	io.add_sync_method(mu_ra_url_name, move |_: Params| {
+	io.add_sync_method("author_getMuRaUrl", move |_: Params| {
 		let url = match GLOBAL_PRIMITIVES_CACHE.get_mu_ra_url() {
 			Ok(url) => url,
 			Err(status) => {
@@ -107,8 +102,7 @@ where
 		Ok(json!(json_value.to_hex()))
 	});
 
-	let untrusted_url_name: &str = "author_getUntrustedUrl";
-	io.add_sync_method(untrusted_url_name, move |_: Params| {
+	io.add_sync_method("author_getUntrustedUrl", move |_: Params| {
 		let url = match GLOBAL_PRIMITIVES_CACHE.get_untrusted_worker_url() {
 			Ok(url) => url,
 			Err(status) => {
@@ -121,31 +115,23 @@ where
 		Ok(json!(json_value.to_hex()))
 	});
 
-	// chain_subscribeAllHeads
-	let chain_subscribe_all_heads_name: &str = "chain_subscribeAllHeads";
-	io.add_sync_method(chain_subscribe_all_heads_name, |_: Params| {
+	io.add_sync_method("chain_subscribeAllHeads", |_: Params| {
 		let parsed = "world";
 		Ok(Value::String(format!("hello, {}", parsed)))
 	});
 
-	// state_getMetadata
-	let state_get_metadata_name: &str = "state_getMetadata";
-	io.add_sync_method(state_get_metadata_name, |_: Params| {
+	io.add_sync_method("state_getMetadata", |_: Params| {
 		let metadata = Runtime::metadata();
 		let json_value = RpcReturnValue::new(metadata.into(), false, DirectRequestStatus::Ok);
 		Ok(json!(json_value.to_hex()))
 	});
 
-	// state_getRuntimeVersion
-	let state_get_runtime_version_name: &str = "state_getRuntimeVersion";
-	io.add_sync_method(state_get_runtime_version_name, |_: Params| {
+	io.add_sync_method("state_getRuntimeVersion", |_: Params| {
 		let parsed = "world";
 		Ok(Value::String(format!("hello, {}", parsed)))
 	});
 
-	// state_executeGetter
-	let state_execute_getter_name: &str = "state_executeGetter";
-	io.add_sync_method(state_execute_getter_name, move |params: Params| {
+	io.add_sync_method("state_executeGetter", move |params: Params| {
 		let json_value = match execute_getter_inner(getter_executor.as_ref(), params) {
 			Ok(state_getter_value) => RpcReturnValue {
 				do_watch: false,
@@ -158,9 +144,7 @@ where
 		Ok(json!(json_value))
 	});
 
-	// attesteer_forward_dcap_quote
-	let attesteer_forward_dcap_quote: &str = "attesteer_forwardDcapQuote";
-	io.add_sync_method(attesteer_forward_dcap_quote, move |params: Params| {
+	io.add_sync_method("attesteer_forwardDcapQuote", move |params: Params| {
 		let json_value = match forward_dcap_quote_inner(params) {
 			Ok(val) => RpcReturnValue {
 				do_watch: false,
@@ -174,9 +158,7 @@ where
 		Ok(json!(json_value))
 	});
 
-	// attesteer_forward_ias_attestation_report
-	let attesteer_forward_ias_attestation_report: &str = "attesteer_forwardIasAttestationReport";
-	io.add_sync_method(attesteer_forward_ias_attestation_report, move |params: Params| {
+	io.add_sync_method("attesteer_forwardIasAttestationReport", move |params: Params| {
 		let json_value = match attesteer_forward_ias_attestation_report_inner(params) {
 			Ok(val) => RpcReturnValue {
 				do_watch: false,
@@ -190,28 +172,21 @@ where
 		Ok(json!(json_value))
 	});
 
-	// system_health
-	let state_health_name: &str = "system_health";
-	io.add_sync_method(state_health_name, |_: Params| {
+	io.add_sync_method("system_health", |_: Params| {
 		let parsed = "world";
 		Ok(Value::String(format!("hello, {}", parsed)))
 	});
 
-	// system_name
-	let state_name_name: &str = "system_name";
-	io.add_sync_method(state_name_name, |_: Params| {
+	io.add_sync_method("system_name", |_: Params| {
 		let parsed = "world";
 		Ok(Value::String(format!("hello, {}", parsed)))
 	});
 
-	// system_version
-	let state_version_name: &str = "system_version";
-	io.add_sync_method(state_version_name, |_: Params| {
+	io.add_sync_method("system_version", |_: Params| {
 		let parsed = "world";
 		Ok(Value::String(format!("hello, {}", parsed)))
 	});
 
-	// returns all rpcs methods
 	let rpc_methods_string = get_all_rpc_methods_string(&io);
 	io.add_sync_method("rpc_methods", move |_: Params| {
 		Ok(Value::String(rpc_methods_string.to_owned()))
