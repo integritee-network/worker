28565de65f291ac8a49e551d31dfb8c25b99d2fc
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-02-23 12:07:40 +0900

    [raffle/stf/trusted_call] cleanup

--- app-libs/raffle/stf/src/trusted_call.rs
+++ app-libs/raffle/stf/src/trusted_call.rs
@@ -15,36 +15,17 @@
 
 */
 
-use codec::{Compact, Decode, Encode};
-use frame_support::{ensure, traits::UnfilteredDispatchable};
+use codec::{Decode, Encode};
+use frame_support::traits::UnfilteredDispatchable;
+use ita_sgx_runtime::Runtime;
 pub use ita_sgx_runtime::{Balance, Index};
-use ita_sgx_runtime::{
-	ParentchainInstanceIntegritee, ParentchainInstanceTargetA, ParentchainInstanceTargetB,
-	ParentchainIntegritee, Runtime, System,
-};
 use itp_node_api::metadata::{provider::AccessNodeMetadata, NodeMetadataTrait};
-use itp_node_api_metadata::{
-	pallet_balances::BalancesCallIndexes, pallet_enclave_bridge::EnclaveBridgeCallIndexes,
-	pallet_proxy::ProxyCallIndexes,
-};
+use itp_node_api_metadata::pallet_enclave_bridge::EnclaveBridgeCallIndexes;
 use itp_stf_interface::ExecuteCall;
-use itp_stf_primitives::{
-	error::StfError,
-	traits::{TrustedCallSigning, TrustedCallVerification},
-	types::{AccountId, KeyPair, ShardIdentifier, Signature, TrustedOperation},
-};
-use itp_types::{
-	parentchain::{ParentchainCall, ParentchainId, ProxyType},
-	Address, Moment, OpaqueCall,
-};
+use itp_stf_primitives::{error::StfError, types::AccountId};
+use itp_types::{parentchain::ParentchainCall, OpaqueCall};
 use itp_utils::stringify::account_id_to_string;
 use log::*;
-use sp_core::{
-	crypto::{AccountId32, UncheckedFrom},
-	ed25519,
-};
-use sp_io::hashing::blake2_256;
-use sp_runtime::{traits::Verify, MultiAddress, MultiSignature};
 use std::{format, prelude::v1::*, sync::Arc};
 
 pub use pallet_raffles::{RaffleCount, RaffleIndex, WinnerCount};
@@ -90,18 +71,26 @@ where
 						Self::Error::Dispatch(format!("Create Raffle error: {:?}", e.error))
 					})?;
 
-				Runtime::read_events().last().map(|event| -> Result<(), Self::Error> {
-					calls.push(ParentchainCall::Integritee(OpaqueCall::from_tuple(&(
-						node_metadata_repo
-							.get_from_metadata(|m| m.publish_hash_call_indexes())
-							.map_err(|_| Self::Error::InvalidMetadata)?
-							.map_err(|_| Self::Error::InvalidMetadata)?,
-						itp_types::H256::default(), // don't bother with the call hash for now.
-						Vec::<itp_types::H256>::new(),
-						format!("{:?}", event),
-					))));
-					Ok(())
-				});
+				Runtime::read_events()
+					.last()
+					.map(|event| -> Result<(), Self::Error> {
+						calls.push(ParentchainCall::Integritee(OpaqueCall::from_tuple(&(
+							node_metadata_repo
+								.get_from_metadata(|m| m.publish_hash_call_indexes())
+								.map_err(|_| Self::Error::InvalidMetadata)?
+								.map_err(|_| Self::Error::InvalidMetadata)?,
+							itp_types::H256::default(), // don't bother with the call hash for now.
+							Vec::<itp_types::H256>::new(),
+							format!("{:?}", event),
+						))));
+						Ok(())
+					})
+					.transpose()?
+					.ok_or_else(|| {
+						Self::Error::Dispatch(format!(
+							"AddRaffle: Could not find expected raffle created event"
+						))
+					})?;
 
 				Ok::<(), Self::Error>(())
 			},
