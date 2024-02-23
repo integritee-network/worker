fe11fa932993841edac7dc3381689fe1a42f7220
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-02-23 13:02:28 +0900

    [raffle-std] correctly show event on parentchain

--- app-libs/raffle/stf/src/trusted_call.rs
+++ app-libs/raffle/stf/src/trusted_call.rs
@@ -26,7 +26,6 @@ use itp_stf_primitives::{error::StfError, types::AccountId};
 use itp_types::{parentchain::ParentchainCall, OpaqueCall};
 use itp_utils::stringify::account_id_to_string;
 use log::*;
-use sp_core::bounded::alloc;
 use sp_std::{sync::Arc, vec::Vec};
 
 #[cfg(not(feature = "std"))]
@@ -75,18 +74,31 @@ where
 						Self::Error::Dispatch(format!("Create Raffle error: {:?}", e.error))
 					})?;
 
+				// Todo: not sure what is better, reading the event or trying to read from storage.
 				Runtime::read_events()
 					.last()
-					.map(|event| -> Result<(), Self::Error> {
-						calls.push(ParentchainCall::Integritee(OpaqueCall::from_tuple(&(
-							node_metadata_repo
-								.get_from_metadata(|m| m.publish_hash_call_indexes())
-								.map_err(|_| Self::Error::InvalidMetadata)?
-								.map_err(|_| Self::Error::InvalidMetadata)?,
-							itp_types::H256::default(), // don't bother with the call hash for now.
-							Vec::<itp_types::H256>::new(),
-							format!("{:?}", event),
-						))));
+					.map(|event_record| -> Result<(), Self::Error> {
+						match &event_record.event {
+							ita_sgx_runtime::RuntimeEvent::Raffles(
+								pallet_raffles::Event::RaffleAdded { index, raffle },
+							) => {
+								calls.push(ParentchainCall::Integritee(OpaqueCall::from_tuple(&(
+									node_metadata_repo
+										.get_from_metadata(|m| m.publish_hash_call_indexes())
+										.map_err(|_| Self::Error::InvalidMetadata)?
+										.map_err(|_| Self::Error::InvalidMetadata)?,
+									itp_types::H256::default(), // don't bother with the call hash for now.
+									Vec::<itp_types::H256>::new(),
+									// Todo: Simple forwarding of the runtime event does not work
+									// as the debug implementation is <wasm:stripped>.
+									format!("Raffle Added: index: {}, raffle: {:?}", index, raffle),
+								))));
+							},
+							_ =>
+								return Err(Self::Error::Dispatch(format!(
+									"AddRaffle: Could not find expected raffle created event"
+								))),
+						}
 						Ok(())
 					})
 					.transpose()?
