83f5a5be8767011a7e1e0aacf88d927178774732
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-11-22 14:24:40 +0100

    remove core

--- app-libs/parentchain-interface/src/lib.rs
+++ app-libs/parentchain-interface/src/lib.rs
@@ -18,7 +18,6 @@
 #![cfg_attr(all(not(target_env = "sgx"), not(feature = "std")), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 
-extern crate core;
 #[cfg(all(not(feature = "std"), feature = "sgx"))]
 extern crate sgx_tstd as std;
 
