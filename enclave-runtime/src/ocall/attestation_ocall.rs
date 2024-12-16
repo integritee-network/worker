60a2e8f0e76a6b1f1b754c21180a8c95788ff16b
brenzi <brenzi@users.noreply.github.com>
2024-12-16 09:19:09 +0100

    remove EPID call for mrenclave of self (#1662)
    * remove EPID call for mrenclave of self
    
    * bump 0.15.5

--- enclave-runtime/src/ocall/attestation_ocall.rs
+++ enclave-runtime/src/ocall/attestation_ocall.rs
@@ -249,10 +249,7 @@ trait GetSgxReport {
 
 impl<T: EnclaveAttestationOCallApi> GetSgxReport for T {
 	fn get_report_of_self(&self) -> SgxResult<sgx_report_body_t> {
-		// (1) get ti + eg
-		let init_quote_result = self.sgx_init_quote()?;
-
-		let target_info = init_quote_result.0;
+		let target_info: sgx_target_info_t = sgx_target_info_t::default();
 		let report_data: sgx_report_data_t = sgx_report_data_t::default();
 
 		let rep = match rsgx_create_report(&target_info, &report_data) {
