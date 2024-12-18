693eff06d249d886d4f3aaeb4682cc78689c44e4
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-12-18 16:19:18 +0100

    Revert "Revert "[enclave-runtime] implement parentchain event handler""
    This reverts commit 5698d957db3ad30f746ccc22b98f6a22673901c2.

--- app-libs/parentchain-interface/src/target_b/event_handler.rs
+++ app-libs/parentchain-interface/src/target_b/event_handler.rs
@@ -14,16 +14,44 @@
 	limitations under the License.
 
 */
-
-pub use ita_sgx_runtime::{Balance, Index};
-
-use ita_stf::TrustedCallSigned;
+use codec::Encode;
+use ita_sgx_runtime::Balance;
+use ita_stf::{Getter, TrustedCall, TrustedCallSigned};
 use itc_parentchain_indirect_calls_executor::error::Error;
-use itp_stf_primitives::traits::IndirectExecutor;
-use itp_types::parentchain::{AccountId, FilterEvents, HandleParentchainEvents};
+use itp_stf_primitives::{traits::IndirectExecutor, types::TrustedOperation};
+use itp_types::parentchain::{
+	AccountId, FilterEvents, HandleParentchainEvents, ParentchainError, ParentchainId,
+};
+use itp_utils::hex::hex_encode;
 use log::*;
 
 pub struct ParentchainEventHandler {}
+
+impl ParentchainEventHandler {
+	fn shield_funds<Executor: IndirectExecutor<TrustedCallSigned, Error>>(
+		executor: &Executor,
+		account: &AccountId,
+		amount: Balance,
+	) -> Result<(), Error> {
+		trace!("[TargetB] shielding for {:?} amount {}", account, amount,);
+		let shard = executor.get_default_shard();
+		// todo: ensure this parentchain is assigned for the shard vault!
+		let trusted_call = TrustedCall::balance_shield(
+			executor.get_enclave_account()?,
+			account.clone(),
+			amount,
+			ParentchainId::TargetB,
+		);
+		let signed_trusted_call = executor.sign_call_with_self(&trusted_call, &shard)?;
+		let trusted_operation =
+			TrustedOperation::<TrustedCallSigned, Getter>::indirect_call(signed_trusted_call);
+
+		let encrypted_trusted_call = executor.encrypt(&trusted_operation.encode())?;
+		executor.submit_trusted_call(shard, encrypted_trusted_call);
+
+		Ok(())
+	}
+}
 
 impl<Executor> HandleParentchainEvents<Executor, TrustedCallSigned, Error>
 	for ParentchainEventHandler
@@ -31,11 +59,25 @@ where
 	Executor: IndirectExecutor<TrustedCallSigned, Error>,
 {
 	fn handle_events(
-		_executor: &Executor,
-		_events: impl FilterEvents,
-		_vault_account: &AccountId,
+		executor: &Executor,
+		events: impl FilterEvents,
+		vault_account: &AccountId,
 	) -> Result<(), Error> {
-		debug!("not handling any events for target B");
+		let filter_events = events.get_transfer_events();
+		trace!(
+			"[TargetB] filtering transfer events to shard vault account: {}",
+			hex_encode(vault_account.encode().as_slice())
+		);
+		if let Ok(events) = filter_events {
+			events
+				.iter()
+				.filter(|&event| event.to == *vault_account)
+				.try_for_each(|event| {
+					info!("[TargetB] found transfer event to shard vault account: {} will shield to {}", event.amount, hex_encode(event.from.encode().as_ref()));
+					Self::shield_funds(executor, &event.from, event.amount)
+				})
+				.map_err(|_| ParentchainError::ShieldFundsFailure)?;
+		}
 		Ok(())
 	}
 }
