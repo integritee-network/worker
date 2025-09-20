affbcb358a77c70e79211114099071ab28326bcc
Alain Brenzikofer <alain@integritee.network>
2025-09-20 21:14:19 +0200

    fix CI

--- cli/demo_send_relayed_note.sh
+++ cli/demo_send_relayed_note.sh
@@ -61,7 +61,7 @@ echo "Using MRENCLAVE: ${MRENCLAVE}"
 TCLIENT="${CLIENT} trusted  --mrenclave ${MRENCLAVE} --direct"
 
 NOTE="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-${TCLIENT} send-note --ipfs-proxy //Alice //Bob ${NOTE}
+${TCLIENT} send-note --ipfs-proxy //Alice //Bob "${NOTE}"
 echo "Alice sent note to Bob:"
 echo $NOTE
 
