a734b70d2a87bffdebfe9786c7c42c38ed1c70a7
Alain Brenzikofer <alain@integritee.network>
2025-09-22 13:30:35 +0200

    probe content size for OCALL

--- enclave-runtime/src/test/ipfs_tests.rs
+++ enclave-runtime/src/test/ipfs_tests.rs
@@ -52,23 +52,25 @@ pub fn test_ocall_read_write_ipfs() {
 }
 
 pub fn test_ocall_write_ipfs_fallback() {
-	info!("testing IPFS write if api is unreachable. Expected to fallback to dump local file...");
-	let enc_state: Vec<u8> = vec![20; 4 * 512 * 1024];
-	let expected_cid = IpfsCid::from_content_bytes(&enc_state).unwrap();
-	let result = OcallApi.write_ipfs(enc_state.as_slice());
+	let payload_sizes = vec![1, 100, 1024];
+	for payload_size in payload_sizes {
+		info!("testing IPFS write of {}kB if api is unreachable. Expected to fallback to dump local file...", payload_size);
+		let enc_state: Vec<u8> = vec![20; payload_size * 1024];
+		let expected_cid = IpfsCid::from_content_bytes(&enc_state).unwrap();
+		let result = OcallApi.write_ipfs(enc_state.as_slice());
 
-	if result.is_ok() {
-		panic!("write_ipfs succeeded, but was expected to fail and fallback to local file dump. Did you accidentally provide an ipfs api url to the test?");
-	} else {
-		let dumpfile =
-			find_first_matching_file(expected_cid.to_string()).expect("dumped file not found");
-		let mut f = fs::File::open(dumpfile).unwrap();
-		let mut content_buf = Vec::new();
-		f.read_to_end(&mut content_buf).unwrap();
-		info!("reading file {:?} of size {} bytes", f, &content_buf.len());
-		let file_cid = IpfsCid::from_content_bytes(&content_buf).unwrap();
-		assert_eq!(expected_cid, file_cid);
-		return
+		if result.is_ok() {
+			panic!("write_ipfs succeeded, but was expected to fail and fallback to local file dump. Did you accidentally provide an ipfs api url to the test?");
+		} else {
+			let dumpfile =
+				find_first_matching_file(expected_cid.to_string()).expect("dumped file not found");
+			let mut f = fs::File::open(dumpfile).unwrap();
+			let mut content_buf = Vec::new();
+			f.read_to_end(&mut content_buf).unwrap();
+			info!("reading file {:?} of size {} bytes", f, &content_buf.len());
+			let file_cid = IpfsCid::from_content_bytes(&content_buf).unwrap();
+			assert_eq!(expected_cid, file_cid);
+		}
 	}
 }
 
